8d3fc26ef571cacd429cdea0ffc7ec783f9c1598
竹内佑介 <kaidouji85@gmail.com>
2021-05-12 09:18:20 +0900

    ボディパーサを導入した

--- packages/monosilic-server/src/index.js
+++ packages/monosilic-server/src/index.js
@@ -1,16 +1,25 @@
 // @flow
 
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import type {User} from "@gbraver-burst-network/core";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
 
 app.get('/', (req, res) => {
   const user: User = {id: 'hello world'};
   res.send(user);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/login', (req, res) => {
+  console.log(req.body);
+  res.send({hp: 1000, power: 2000});
 });
