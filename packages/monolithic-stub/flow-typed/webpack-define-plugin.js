dfbeee0e353ac5a5c85e2b60ed69fcb57532d7bb
竹内佑介 <kaidouji85@gmail.com>
2021-05-18 08:34:43 +0900

    スタブに.envを導入した

---
+++ packages/monolithic-stub/flow-typed/webpack-define-plugin.js
@@ -0,0 +1,7 @@
+/**
+ * webpack.config.js Webpack Define Pluginで定義したグローバル変数
+ */
+
+declare var API_SERVER_UTL: string;
+declare var USER_ID: string;
+declare var PASSWORD: string;
--- packages/monolithic-stub/src/index.js
+++ packages/monolithic-stub/src/index.js
@@ -1,9 +1,11 @@
 // @flow
 
-import {StubSelector} from "./stub-selector";
-import {createStubContainer} from "./stub-container";
+import {StubSelector} from "./stub/stub-selector";
+import {createStubContainer} from "./stub/stub-container";
+import {WebpackDefinePlugin} from './webpack-define-plugin';
 
 window.onload = () => {
+  console.log(WebpackDefinePlugin);
   const stubContainer = createStubContainer('http://localhost:3000', 'user', 'pass');
   const stubSelector = new StubSelector(stubContainer);
   const stubSelectorBinder: HTMLElement = document.querySelector('#stub-selector')
