2d3a4a9bbf9e0f544b17d44ba58d79417ef71375
竹内佑介 <kaidouji85@gmail.com>
2021-05-24 14:43:10 +0900

    カジュアルマッチスタートのスタブを作成した

--- packages/monolithic-browser/src/index.js
+++ packages/monolithic-browser/src/index.js
@@ -1,14 +1,16 @@
 // @flow
 
 import io from 'socket.io-client';
-import type {IdPasswordLogin, LoginCheck, UserID} from "@gbraver-burst-network/core";
+import type {BattleRoom, CasualMatch, IdPasswordLogin, LoginCheck, UserID} from "@gbraver-burst-network/core";
 import {isLogin, login} from "./login";
+import type {ArmDozerId, Command, GameState, PilotId, Player} from "gbraver-burst-core";
+import {ArmDozerIdList, ArmDozers, PilotIds, Pilots} from "gbraver-burst-core";
 
 /** モノシリックサーバ ブラウザ用 SDK */
-export class MonolithicBrowser implements IdPasswordLogin, LoginCheck {
+export class MonolithicBrowser implements IdPasswordLogin, LoginCheck, CasualMatch {
   _apiServerURL: string
   _accessToken: string;
-  _socket: typeof io.Socket;
+  _socket: typeof io.Socket | null;
 
   /**
    * コンストラクタ
@@ -18,8 +20,7 @@ export class MonolithicBrowser implements IdPasswordLogin, LoginCheck {
   constructor(apiServerURL: string) {
     this._apiServerURL = apiServerURL;
     this._accessToken = '';
-    this._socket = io(apiServerURL);
-    this._socket.on('connect_error', this._onSocketError.bind(this));
+    this._socket = null;
   }
 
   /**
@@ -50,7 +51,52 @@ export class MonolithicBrowser implements IdPasswordLogin, LoginCheck {
     return isLogin(this._accessToken, this._apiServerURL);
   }
 
-  _onSocketError(err: string): void {
-    console.error(err);
+  /**
+   * カジュアルマッチをスタートさせる
+   *
+   * @param armdozerId 選択したアームドーザID
+   * @param pilotId 選択したパイロットID
+   * @return バトルルーム準備
+   */
+  startCasualMatch(armdozerId: ArmDozerId, pilotId: PilotId): Promise<BattleRoom> {
+    console.log(armdozerId, pilotId);
+    return Promise.resolve(emptyBattleRoom());
   }
+}
+
+/**
+ * 空のバトルルームを生成する
+ *
+ * @return 生成結果
+ */
+export function emptyBattleRoom(): BattleRoom {
+  const shinBraver = ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER)
+    ?? ArmDozers[0];
+  const neoLandozer = ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER)
+    ?? ArmDozers[0];
+  const shinya = Pilots.find(v => v.id === PilotIds.SHINYA)
+    ?? Pilots[0];
+  const gai = Pilots.find(v => v.id === PilotIds.GAI)
+    ?? Pilots[0];
+
+  const player: Player = {
+    playerId: 'player',
+    armdozer: shinBraver,
+    pilot: shinya
+  };
+  const enemy: Player = {
+    playerId: 'enemy',
+    armdozer: neoLandozer,
+    pilot: gai
+  };
+
+  return {
+    player: player,
+    enemy: enemy,
+    initialState: [],
+    progress: (command: Command): Promise<GameState[]> => {
+      console.log(command);
+      return Promise.resolve([]);
+    }
+  };
 }
