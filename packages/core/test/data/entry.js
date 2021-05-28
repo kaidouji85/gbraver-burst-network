b8aec41e1c1b68af9c84520e455159fb2ac57cda
竹内佑介 <kaidouji85@gmail.com>
2021-05-28 09:48:31 +0900

    coreにユニットテストを導入した

---
+++ packages/core/test/data/entry.js
@@ -0,0 +1,10 @@
+// @flow
+
+import type {Entry} from "../../src/waiting-room/entry";
+
+/** 空のエントリ */
+export const EMPTY_ENTRY: Entry = {
+  userID: '',
+  armdozerId: '',
+  pilotId: ''
+};
---
+++ packages/core/test/src/waiting-room/is-double-entry-test.js
@@ -0,0 +1,15 @@
+// @flow
+
+import test from 'ava';
+import {EMPTY_ENTRY} from "../../data/entry";
+import {isDoubleEntry} from "../../../src/waiting-room/is-double-entry";
+
+test('同じユーザIDでエントリした場合、二重入室と判定する', t => {
+  const roomEntries = [
+    {...EMPTY_ENTRY, userID: 'test01'},
+    {...EMPTY_ENTRY, userID: 'test02'},
+  ];
+  const entry = {...EMPTY_ENTRY, userID: 'test02'};
+  const result = isDoubleEntry(roomEntries, entry.userID);
+  t.true(result);
+});
