16848fbe73e1c2a71ac50df7ca1fab0dcea64476
竹内佑介 <kaidouji85@gmail.com>
2021-06-05 21:58:29 +0900

    セッションコンテナを追加した

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,7 +2,14 @@
 
 export type {User, UserID} from './user';
 export type {IdPasswordLogin, LoginCheck} from './user-operation/login';
+
+export type {Session} from './session/session';
+export {createSessionFromUser} from './session/create-session';
+export {SessionContainer} from './session/session-container';
+export type {AddSession, AllSessions} from './session/session-container';
+
+export {BattleRoom} from './battle-room/battle-room';
+export type {InputCommandResult, Progress, Waiting} from "./battle-room/battle-room";
+
 export type {CasualMatch} from './user-operation/casual-match';
 export type {Battle} from './user-operation/battle';
-export type {Session} from './session/session';
-export type {createSessionFromUser} from './session/create-session';
