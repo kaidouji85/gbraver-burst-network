23da1b1c863e6b566f5af4cb0fceaf4d0f2a33ca
竹内佑介 <kaidouji85@gmail.com>
2021-06-08 23:13:05 +0900

    カジュアルマッチ関数にJSDocを追加した

--- packages/monolithic-browser/src/start-casual-match.js
+++ packages/monolithic-browser/src/start-casual-match.js
@@ -3,6 +3,14 @@
 import {Socket} from 'socket.io-client';
 import type {ArmDozerId, PilotId} from "gbraver-burst-core";
 
+/**
+ * カジュアルマッチを開始する
+ *
+ * @param socket カジュアルマッチを開始するソケット
+ * @param armdozerID 選択したアームドーザID
+ * @param pilotId 選択したパイロットID
+ * @return 結果
+ */
 export function startCasualMatch(socket: typeof Socket, armdozerID: ArmDozerId, pilotId: PilotId): Promise<void> {
   return new Promise(resolve => {
     const data = {armdozerId: armdozerID, pilotId: pilotId};
@@ -10,5 +18,7 @@ export function startCasualMatch(socket: typeof Socket, armdozerID: ArmDozerId, 
     socket.once('Matching', () => {
       resolve();
     });
+  }).finally(() => {
+    socket.removeAllListeners();
   });
 }
