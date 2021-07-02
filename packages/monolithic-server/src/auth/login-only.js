f4f347581e590f65ffadcb3f2b2bdb17facada06
竹内佑介 <kaidouji85@gmail.com>
2021-07-02 10:15:40 +0900

    ログインチェックでトークンデコード例外が出る不具合を修正した

--- packages/monolithic-server/src/auth/login-only.js
+++ packages/monolithic-server/src/auth/login-only.js
@@ -27,7 +27,7 @@ export const loginOnlyForExpress = (accessToken: AccessTokenPayloadParser, sessi
     }
   
     const splitHeader = authHeader.split(' ');
-    if ((splitHeader.length !== 2) && (splitHeader[0] !== 'Bearer')) {
+    if ((splitHeader.length !== 2) || (splitHeader[0] !== 'Bearer') || (splitHeader[1] === '')) {
       res.sendStatus(401);
       return;
     }
@@ -62,7 +62,7 @@ export const loginOnlyForSocketIO = (accessToken: AccessTokenPayloadParser, sess
   const invalidAccessToken = new Error('invalid access token');
   try {
     const token = socket.handshake?.auth?.token;
-    if (!token || (typeof token !== 'string')) {
+    if (!token || (typeof token !== 'string') || token === '') {
       next(invalidAccessToken);
       return;
     }
