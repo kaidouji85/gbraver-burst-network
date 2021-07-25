37ce27509e248dd28c068e5227ceb298ba76f567
竹内佑介 <kaidouji85@gmail.com>
2021-07-25 12:25:50 +0900

    環境変数から設定値を取得するヘルパー関数を削除した

--- packages/monolithic-server/src/local.js
+++ packages/monolithic-server/src/local.js
@@ -1,15 +1,13 @@
 // @flow
 import dotenv from 'dotenv';
 import {monolithicServer} from './app/monolithic-server';
-import {listenPortFromEnv} from "./port/listen-port";
 import {UsersFromJSON} from "./users/users-from-json";
-import {accessTokenSecretFromEnv} from "./auth/access-token-secret";
 
 dotenv.config();
 
-monolithicServer({
-  listenPort: listenPortFromEnv(),
-  accessControllOrigin: process.env.ACCESS_CONTROL_ALLOW_ORIGIN ?? '',
-  accessTokenSecret: accessTokenSecretFromEnv(),
-  users: new UsersFromJSON('users.json'),
-});
+const portFromEnv = parseInt(process.env.LISTEN_PORT);
+const listenPort = isNaN(portFromEnv) ? 4000 : portFromEnv;
+const accessControlOrigin = process.env.ACCESS_CONTROL_ALLOW_ORIGIN ?? '';
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET ?? '';
+const users = new UsersFromJSON('users.json');
+monolithicServer({listenPort, accessControlOrigin, accessTokenSecret, users});
