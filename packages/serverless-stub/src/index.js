a6e9f19dc81134eef8e968e51d60acfb7f399853
Yuusuke <kaidouji85@gmail.com>
2021-08-17 11:08:05 +0000

    APIスタブでログイン、ログアウトができるようになった

--- packages/serverless-stub/src/index.js
+++ packages/serverless-stub/src/index.js
@@ -5,10 +5,10 @@ import {createBrowserSDK} from '@gbraver-burst-network/browser-sdk';
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? '';
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID ?? '';
 const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? '';
-const API_URL = process.env.API_URL ?? '';
+//const API_URL = process.env.API_URL ?? '';
 
 window.onload = async () => {
-  const browserSDK = await createBrowserSDK(AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE, API_URL);
+  const browserSDK = await createBrowserSDK(AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE, window.location.origin);
   if (browserSDK.isLoginSuccessRedirect()) {
     await browserSDK.afterLoginSuccess();
   }
@@ -30,4 +30,6 @@ window.onload = async () => {
     await browserSDK.logout();
     await updateScreen();
   });
+
+  updateScreen();
 };
