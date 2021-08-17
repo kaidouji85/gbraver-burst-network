b484704e56d8fe7c743ecd16383540126d6e7d7f
竹内佑介 <kaidouji85@gmail.com>
2021-08-18 01:50:20 +0900

    ビルドが動かない不具合を修正した

--- packages/serverless-stub/webpack.config.js
+++ packages/serverless-stub/webpack.config.js
@@ -39,11 +39,11 @@ module.exports = {
       template: 'src/index.html',
       inject: true
     }),
-    new webpack.EnvironmentPlugin([
-      'AUTH0_DOMAIN',
-      'AUTH0_CLIENT_ID',
-      'AUTH0_AUDIENCE',
-      'API_URL',
-    ])
+    new webpack.DefinePlugin({
+      'process.env.AUTH0_DOMAIN': JSON.stringify(process.env.AUTH0_DOMAIN),
+      'process.env.AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID),
+      'process.env.AUTH0_AUDIENCE': JSON.stringify(process.env.AUTH0_AUDIENCE),
+      'process.env.API_URL': JSON.stringify(process.env.API_URL),
+    })
   ]
 };
