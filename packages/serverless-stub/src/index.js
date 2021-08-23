065fed38e460e83222267fc83ee63b6eaf8107b5
竹内佑介 <kaidouji85@gmail.com>
2021-08-23 15:55:28 +0900

    スタブにユースケースの雛形を追加した

--- packages/serverless-stub/src/index.js
+++ packages/serverless-stub/src/index.js
@@ -1,27 +1,35 @@
 // @flow
 
 import {createBrowserSDK} from '@gbraver-burst-network/browser-sdk';
+import type {UseCase} from "./use-case/use-case";
+import {PingUseCase} from "./use-case/ping";
 
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? '';
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID ?? '';
 const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? '';
-//const API_URL = process.env.API_URL ?? '';
+const API_URL = process.env.API_URL ?? '';
 
 window.onload = async () => {
-  const browserSDK = await createBrowserSDK(AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE, window.location.origin);
+  const browserSDK = await createBrowserSDK(window.location.origin, API_URL, AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE);
   if (browserSDK.isLoginSuccessRedirect()) {
     await browserSDK.afterLoginSuccess();
   }
 
+  const useCases: UseCase[] = [
+    new PingUseCase(browserSDK)
+  ];
+  console.log(useCases);
   const loginForm = document.getElementById('login-form') ?? document.createElement('form');
   const loginButton = document.getElementById('login-button') ?? document.createElement('button');
   const logoutForm = document.getElementById('logout-form') ?? document.createElement('form');
   const logoutButton = document.getElementById('logout-button') ?? document.createElement('button');
+  const useCaseForm = document.getElementById('use-case-form') ?? document.createElement('form');
 
   const updateScreen = async () => {
     const isLogin = await browserSDK.isLogin();
     loginForm.style.display = isLogin ? 'none' : 'block';
     logoutForm.style.display = isLogin ? 'block' : 'none';
+    useCaseForm.style.display = isLogin ? 'block' : 'none';
   };
   loginButton.addEventListener('click', async () => {
     await browserSDK.gotoLoginPage();
