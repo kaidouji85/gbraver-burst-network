6e415e8985870e5703e6f9a2963b075a06dd04c1
竹内佑介 <kaidouji85@gmail.com>
2021-08-28 19:10:14 +0900

    pingレスポンス呼び出しヘルパー関数のレスポンスを変更した

--- packages/browser-sdk/src/websocket/ping.js
+++ packages/browser-sdk/src/websocket/ping.js
@@ -4,6 +4,7 @@ import {parsePingResponse} from "./response";
 import {onMessage} from "./message";
 import type {Resolve} from "../promise/promise";
 import {parseJSON} from "../json/parse";
+import type {PingResponse} from "./response";
 
 /**
  * API サーバへの疎通確認
@@ -11,11 +12,11 @@ import {parseJSON} from "../json/parse";
  * @param websocket websocketクライアント
  * @return APIサーバからの返答内容
  */
-export function ping(websocket: WebSocket): Promise<string> {
+export function ping(websocket: WebSocket): Promise<PingResponse> {
   websocket.send(JSON.stringify({action: 'ping'}));
-  return onMessage(websocket, (e: MessageEvent, resolve: Resolve<string>): void => {
+  return onMessage(websocket, (e: MessageEvent, resolve: Resolve<PingResponse>): void => {
     const data = parseJSON(e.data);
     const response = parsePingResponse(data);
-    response && resolve(response.message);
+    response && resolve(response);
   });
 }
