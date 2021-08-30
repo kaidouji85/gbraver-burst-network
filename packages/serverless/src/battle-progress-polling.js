411a9ead7a9d2d311ba22f3dbff444dc9ee9c853
竹内佑介 <kaidouji85@gmail.com>
2021-08-30 17:06:30 +0900

    DynamoDBのバトル、リクエストボディの検証コードを追加した

--- packages/serverless/src/battle-progress-polling.js
+++ packages/serverless/src/battle-progress-polling.js
@@ -6,12 +6,14 @@ import {parseBattleProgressPolling} from "./lambda/battle-progress-polling";
 import {parseJSON} from "./json/parse";
 import {createDynamoDBClient} from "./dynamo-db/client";
 import {Battles} from "./dynamo-db/battles";
+import {extractUser} from "./lambda/websocket-api-event";
 
 const AWS_REGION = process.env.AWS_REGION ?? '';
 const BATTLES = process.env.BATTLES ?? '';
 
 const dynamoDB = createDynamoDBClient(AWS_REGION);
 const battles = new Battles(dynamoDB, BATTLES);
+const invalidRequestBody = {statusCode: 400, body: 'invalid request body'};
 
 /**
  * バトル更新用のポーリング
@@ -25,14 +27,22 @@ export async function battleProgressPolling(event: WebsocketAPIEvent): Promise<W
   const body = parseJSON(event.body);
   const data = parseBattleProgressPolling(body);
   if (!data) {
-    return {statusCode: 400, body: 'invalid request body'};
+    return invalidRequestBody;
   }
 
   const battle = await battles.get(data.battleID);
   if (!battle) {
-    return {statusCode: 400, body: 'battle not found'};
+    return invalidRequestBody;
   }
-  console.log(battle);
 
+  const user = extractUser(event.requestContext.authorizer);
+  const isIncludedPlayer = battle.players.map(v => v.userID)
+    .includes(user.userID);
+  const isValidFlowID = data.flowID === battle.flowID;
+  if (!isIncludedPlayer || !isValidFlowID) {
+    return invalidRequestBody;
+  }
+
+  console.log('command is valid', battle);
   return {statusCode: 200, body: 'send command success'};
 }
