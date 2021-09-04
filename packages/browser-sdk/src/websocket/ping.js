5557586d718cf644ccd8f2337feed2d96775050a
竹内佑介 <kaidouji85@gmail.com>
2021-09-04 13:18:59 +0900

    websocketのメッセージ送信ヘルパー関数を追加した

--- packages/browser-sdk/src/websocket/ping.js
+++ packages/browser-sdk/src/websocket/ping.js
@@ -5,6 +5,7 @@ import type {Resolve} from "../promise/promise";
 import {parseJSON} from "../json/parse";
 import type {Pong} from "../response/pong";
 import {parsePong} from "../response/pong";
+import {sendToAPIServer} from "./send-to-api-server";
 
 /**
  * API サーバへの疎通確認
@@ -13,8 +14,7 @@ import {parsePong} from "../response/pong";
  * @return APIサーバからの返答内容
  */
 export function ping(websocket: WebSocket): Promise<Pong> {
-  const data = {action: 'ping'};
-  websocket.send(JSON.stringify(data));
+  sendToAPIServer(websocket, {action: 'ping'});
   return onMessage(websocket, (e: MessageEvent, resolve: Resolve<Pong>): void => {
     const data = parseJSON(e.data);
     const response = parsePong(data);
