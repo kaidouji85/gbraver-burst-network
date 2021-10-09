56e26b1a1d809bf776e63efa062f9f3926dbd2cc
竹内佑介 <kaidouji85@gmail.com>
2021-10-09 22:04:14 +0900

    VPCを追加した

--- packages/aws-infrastructure/lib/aws-infrastructure-stack.ts
+++ packages/aws-infrastructure/lib/aws-infrastructure-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import {SecurityGroup} from "@aws-cdk/aws-ec2";
 
 export class AwsInfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "VPC", {
+      cidr: '192.168.0.0/16',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ],
+    });
+    const securityGroup = new SecurityGroup(this, "SecurityGroup", {
+      vpc: vpc,
+    });
   }
 }
