d994c420e075870effc28c4c1636eeba4f838caa
竹内佑介 <kaidouji85@gmail.com>
2021-10-10 12:11:25 +0900

    NAGゲートウェイ+Fargate環境を構築した

--- packages/aws-infrastructure/bin/aws-infrastructure.ts
+++ packages/aws-infrastructure/bin/aws-infrastructure.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { AwsInfrastructureStack } from '../lib/aws-infrastructure-stack';
 
 const app = new cdk.App();
-new AwsInfrastructureStack(app, 'gbraver-burst-aws-infrastructure', {
+new AwsInfrastructureStack(app, 'study-nat-gateway-match-make', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- packages/aws-infrastructure/lib/aws-infrastructure-stack.ts
+++ packages/aws-infrastructure/lib/aws-infrastructure-stack.ts
@@ -1,23 +1,67 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
-import {SecurityGroup} from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecr from "@aws-cdk/aws-ecr";
+import * as iam from '@aws-cdk/aws-iam';
 
 export class AwsInfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "VPC", {
-      cidr: '192.168.0.0/16',
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: "public-subnet",
-          subnetType: ec2.SubnetType.PUBLIC,
-        }
+    const stage = new cdk.CfnParameter(this, 'stage', {
+      type: 'String',
+      description: 'serverless stage name',
+    }).valueAsString;
+    const websocketAPIID = cdk.Fn.importValue(`gbraver-burst-serverless:${stage}:WebsoketApiId`);
+    const connectionsTableARN = cdk.Fn.importValue(`gbraver-burst-serverless:${stage}:ConnectionsTableArn`);
+    const casualMatchEntriesTableARN = cdk.Fn.importValue(`gbraver-burst-serverless:${stage}:CasualMatchEntriesTableArn`);
+    const battlesTableARN = cdk.Fn.importValue(`gbraver-burst-serverless:${stage}:BattlesTableArn`);
+
+    const vpc = new ec2.Vpc(this, "vpc", {
+      maxAzs: 1,
+    });
+
+    const matchMakeRepository = ecr.Repository.fromRepositoryName(this, "repo", "gbraver-burst-match-make");
+    const matchMakePolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          resources: [connectionsTableARN, casualMatchEntriesTableARN, battlesTableARN],
+          actions: [
+            'dynamodb:PutItem',
+            'dynamodb:GetItem',
+            'dynamodb:DeleteItem',
+            'dynamodb:Scan',
+            'dynamodb:BatchWrite*',
+          ],
+        })
       ],
     });
-    const securityGroup = new SecurityGroup(this, "SecurityGroup", {
-      vpc: vpc,
+    const matchMakeServiceTaskRole = new iam.Role(this, 'match-make-service-task-role', {
+      roleName: 'ecs-service-task-role',
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      inlinePolicies: {matchMakePolicy}
+    });
+    const matchMakeTaskDefinition = new ecs.TaskDefinition(this, "taskdef", {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: "256",
+      memoryMiB: "512",
+      taskRole: matchMakeServiceTaskRole
+    });
+    const matchMakeLogging = new ecs.AwsLogDriver({
+      streamPrefix: "gbraver-burst-match-make",
+    })
+    matchMakeTaskDefinition.addContainer("demo-container", {
+      image: ecs.ContainerImage.fromEcrRepository(matchMakeRepository),
+      environment: {
+        STAGE: stage,
+        WEBSOCKET_API_ID: websocketAPIID,
+      },
+      logging: matchMakeLogging,
+    });
+    const cluster = new ecs.Cluster(this, "cluster", { vpc });
+    const matchMakeService = new ecs.FargateService(this, "service", {
+      cluster,
+      taskDefinition: matchMakeTaskDefinition
     });
   }
 }
