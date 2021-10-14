53cc642715b0b161718739587aa0d3e879ef76f3
Yuusuke <kaidouji85@gmail.com>
2021-10-14 10:41:25 +0000

    シェルの文法ミスを修正した

--- match-make-container-push.sh
+++ match-make-container-push.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if　[ -z "$DOCKER_USER" ] || [ -z "$DOCKER_TOKEN" ] || [ -z "$AWS_DEFAULT_REGION"] || [ -z "$MATCH_MAKE_ECR_REPOSITORY_NAME" ]; then
+if　[ -z "$DOCKER_USER" ] || [ -z "$DOCKER_TOKEN" ] || [ -z "$AWS_DEFAULT_REGION" ] || [ -z "$MATCH_MAKE_ECR_REPOSITORY_NAME" ]; then
   echo 'required environment variables are not defined'
   exit 1
 fi
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin
-IMAGE_TAG=latest
-docker build -t $MATCH_MAKE_ECR_REPOSITORY_NAME:$IMAGE_TAG -f matchMake.Dockerfile .
-docker tag $MATCH_MAKE_ECR_REPOSITORY_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+#aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+#echo $DOCKER_TOKEN | docker login -u $DOCKER_USER --password-stdin
+#IMAGE_TAG=latest
+#docker build -t $MATCH_MAKE_ECR_REPOSITORY_NAME:$IMAGE_TAG -f matchMake.Dockerfile .
+#docker tag $MATCH_MAKE_ECR_REPOSITORY_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
