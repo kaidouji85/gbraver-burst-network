171edb5991f2eb15a8aa0c96fb610800c34900bd
竹内佑介 <kaidouji85@gmail.com>
2021-11-06 15:33:15 +0900

    認証を復活させた

--- packages/browser-sdk/src/http-request/delete-logged-in-account.js
+++ packages/browser-sdk/src/http-request/delete-logged-in-account.js
@@ -1,5 +1,11 @@
 // @flow
 
+/**
+ * 現在ログインしているアカウントを削除する
+ *
+ * @param restAPIURL Rest API のURL
+ * @return 処理が完了したら発火するPromise
+ */
 export async function deleteLoggedInAccount(restAPIURL: string): Promise<void> {
   await fetch(`${restAPIURL}`, {
     mode: 'cors',
