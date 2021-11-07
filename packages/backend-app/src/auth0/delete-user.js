509195241c1c439856310c6830687148a1d0b62c
竹内佑介 <kaidouji85@gmail.com>
2021-11-07 11:19:34 +0900

    一次退避

---
+++ packages/backend-app/src/auth0/delete-user.js
@@ -0,0 +1,22 @@
+// @flow
+
+import {ManagementClient} from 'auth0';
+
+/**
+ * 指定したユーザをauth0から削除する
+ *
+ * @param domain auth0ドメイン
+ * @param clientID Auth0 Management API に接続したApplicationのclient id
+ * @param clientSecret Auth0 Management API に接続したApplicationのclient secret
+ * @param userID 削除するユーザID
+ * @return 処理が完了したら発火するPromise
+ */
+export async function deleteUser(domain: string, clientID: string, clientSecret: string, userID: string): Promise<void> {
+  const management = new ManagementClient({
+    domain: domain,
+    clientId: clientID,
+    clientSecret: clientSecret,
+    scope: 'delete:users'
+  });
+  await management.deleteUser({id: userID});
+}
--- packages/backend-app/src/delete-account.js
+++ packages/backend-app/src/delete-account.js
@@ -4,6 +4,11 @@
 import type {RestAPIResponse} from "./lambda/rest-api-response";
 import type {RestAPIEvent} from "./lambda/rest-api-event";
 import {extractUserFromRestAPIJWT} from "./lambda/extract-user";
+import {deleteUser} from "./auth0/delete-user";
+
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? '';
+const AUTH0_USER_MANAGEMENT_APP_CLIENT_ID = process.env.AUTH0_DOMAIN ?? '';
+const AUTH0_USER_MANAGEMENT_APP_CLIENT_SECRET = process.env.AUTH0_DOMAIN ?? '';
 
 /**
  * アカウント削除API
@@ -13,9 +18,12 @@ import {extractUserFromRestAPIJWT} from "./lambda/extract-user";
  */
 export async function deleteAccount(event: RestAPIEvent): Promise<RestAPIResponse> {
   const user = extractUserFromRestAPIJWT(event.requestContext.authorizer.jwt.claims);
-  console.log(user);
+  // auth0ユーザ削除関数にGブレイバーバーストのユーザIDを指定しているが、
+  // 現状ではauth0、GブレイバーバーストのユーザIDは完全一致するので問題ない
+  await deleteUser(AUTH0_DOMAIN, AUTH0_USER_MANAGEMENT_APP_CLIENT_ID,
+    AUTH0_USER_MANAGEMENT_APP_CLIENT_SECRET, user.userID);
   return {
     statusCode: 200,
-    body: 'delete user success',
+    body: 'delete account success',
   };
 }
