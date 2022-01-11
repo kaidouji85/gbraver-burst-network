df2f72e3eaeb1e12070555a860f8e93dee803a90
Y.Takeuchi <kaidouji85@gmail.com>
2022-01-12 07:41:30 +0900

    WIP avaからjestに移行中
    ユニットテストのjest移行が完了していないので、ビルドパイプラインはエラーで落ちる

--- packages/browser-sdk/.eslintrc.js
+++ packages/browser-sdk/.eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-    parser: "@babel/eslint-parser",
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:flowtype/recommended"
-    ],
-    plugins: [
-        "flowtype"
-    ]
+  parser: "@babel/eslint-parser",
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:flowtype/recommended"
+  ],
+  plugins: [
+    "flowtype",
+    "jest"
+  ]
 };
