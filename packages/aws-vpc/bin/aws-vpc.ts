f0c8611ae241befbd2f7d0496ac63747a1de6300
kaidouji85 <kaidouji85@gmail.com>
2022-10-29 20:38:29 +0900

    改行削除

--- packages/aws-vpc/bin/aws-vpc.ts
+++ packages/aws-vpc/bin/aws-vpc.ts
@@ -7,10 +7,8 @@ import * as dotenv from "dotenv";
 import { AwsVpcStack } from "../lib/aws-vpc-stack";
 
 dotenv.config();
-
 const service = process.env.SERVICE ?? "";
 const cidr = process.env.CIDR ?? "";
 const stackID = `${service}-vpc`;
-
 const app = new App();
 new AwsVpcStack(app, stackID, { service, cidr });
