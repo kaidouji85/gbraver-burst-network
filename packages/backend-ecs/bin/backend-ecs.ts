46b6810f1685153fcc6eb80f33d221dd93ac4c97
kaidouji85 <kaidouji85@gmail.com>
2022-10-29 20:42:54 +0900

    backend-ecsにprettierを追加

--- packages/backend-ecs/bin/backend-ecs.ts
+++ packages/backend-ecs/bin/backend-ecs.ts
@@ -1,25 +1,42 @@
 #!/usr/bin/env node
-import * as dotenv from 'dotenv';
-import 'source-map-support/register';
-import {App, Fn} from 'aws-cdk-lib';
-import {v4 as uuidV4}from 'uuid';
-import {BackendEcsStack} from '../lib/backend-ecs-stack';
+import * as dotenv from "dotenv";
+import "source-map-support/register";
+import { App, Fn } from "aws-cdk-lib";
+import { v4 as uuidV4 } from "uuid";
+import { BackendEcsStack } from "../lib/backend-ecs-stack";
 
 dotenv.config();
 
-const service = process.env.SERVICE ?? '';
-const stage = process.env.STAGE ?? 'dev';
-const matchMakeEcrRepositoryName = process.env.MATCH_MAKE_ECR_REPOSITORY_NAME ?? '';
+const service = process.env.SERVICE ?? "";
+const stage = process.env.STAGE ?? "dev";
+const matchMakeEcrRepositoryName =
+  process.env.MATCH_MAKE_ECR_REPOSITORY_NAME ?? "";
 const vpcId = Fn.importValue(`${service}:VpcId`);
-const publicSubnetAvailabilityZone = Fn.importValue(`${service}:PulicNetAvailabilityZone`);
+const publicSubnetAvailabilityZone = Fn.importValue(
+  `${service}:PulicNetAvailabilityZone`
+);
 const publicSubnetId = Fn.importValue(`${service}:PulicSubnetId`);
 const websocketAPIID = Fn.importValue(`${service}:${stage}:WebsoketApiId`);
-const connectionsTableARN = Fn.importValue(`${service}:${stage}:ConnectionsTableArn`);
-const casualMatchEntriesTableARN = Fn.importValue(`${service}:${stage}:CasualMatchEntriesTableArn`);
+const connectionsTableARN = Fn.importValue(
+  `${service}:${stage}:ConnectionsTableArn`
+);
+const casualMatchEntriesTableARN = Fn.importValue(
+  `${service}:${stage}:CasualMatchEntriesTableArn`
+);
 const battlesTableARN = Fn.importValue(`${service}:${stage}:BattlesTableArn`);
 const uuid = uuidV4();
 
 const app = new App();
-new BackendEcsStack(app, `${service}-${stage}-backend-ecs`, {service, stage, vpcId, publicSubnetAvailabilityZone, publicSubnetId,
-  websocketAPIID, connectionsTableARN, casualMatchEntriesTableARN, battlesTableARN, matchMakeEcrRepositoryName, uuid
+new BackendEcsStack(app, `${service}-${stage}-backend-ecs`, {
+  service,
+  stage,
+  vpcId,
+  publicSubnetAvailabilityZone,
+  publicSubnetId,
+  websocketAPIID,
+  connectionsTableARN,
+  casualMatchEntriesTableARN,
+  battlesTableARN,
+  matchMakeEcrRepositoryName,
+  uuid,
 });
