bf431a26bca18b9dc04d4fc7a5f8cf1c7b830792
kaidouji85 <kaidouji85@gmail.com>
2022-10-29 16:31:27 +0900

    prettier導入

--- packages/aws-vpc/lib/aws-vpc-stack.ts
+++ packages/aws-vpc/lib/aws-vpc-stack.ts
@@ -1,14 +1,14 @@
-import {Construct} from 'constructs';
-import {Stack, StackProps, CfnOutput} from 'aws-cdk-lib';
-import {aws_ec2 as ec2} from 'aws-cdk-lib';
+import { Construct } from "constructs";
+import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
 
 /** VPCスタックプロパティ */
 interface VPCProps extends StackProps {
   /** サービス名 */
-  service: string,
+  service: string;
   /** VPC CIDR */
-  cidr: string
-};
+  cidr: string;
+}
 
 /** VPC スタック */
 export class AwsVpcStack extends Stack {
@@ -23,20 +23,20 @@ export class AwsVpcStack extends Stack {
     const vpc = new ec2.Vpc(this, "vpc", {
       cidr: props.cidr,
       maxAzs: 1,
-      natGateways: 0
+      natGateways: 0,
     });
-    new CfnOutput(this, 'VpcId', {
+    new CfnOutput(this, "VpcId", {
       value: vpc.vpcId,
-      exportName: `${props.service}:VpcId`
+      exportName: `${props.service}:VpcId`,
     });
     const publicSubnet = vpc.publicSubnets[0];
-    new CfnOutput(this, 'PulicNetAvailabilityZone', {
+    new CfnOutput(this, "PulicNetAvailabilityZone", {
       value: publicSubnet.availabilityZone,
-      exportName: `${props.service}:PulicNetAvailabilityZone`
+      exportName: `${props.service}:PulicNetAvailabilityZone`,
     });
-    new CfnOutput(this, 'PulicSubnetId', {
+    new CfnOutput(this, "PulicSubnetId", {
       value: publicSubnet.subnetId,
-      exportName: `${props.service}:PulicSubnetId`
+      exportName: `${props.service}:PulicSubnetId`,
     });
   }
 }
