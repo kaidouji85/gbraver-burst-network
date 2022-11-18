63fbde836399c55e65b905e79b47844887f8b737
kaidouji85 <kaidouji85@gmail.com>
2022-11-19 00:18:54 +0900

    3つのVPCが生成できるようになった

--- packages/aws-vpc/bin/aws-vpc.ts
+++ packages/aws-vpc/bin/aws-vpc.ts
@@ -8,15 +8,21 @@ import { AwsVpcStack } from "../lib/aws-vpc-stack";
 
 /**
  * VCP世代番号、大きい程新しい
- * VPCはめったに更新されないため、backend-app、backend-ecsの世代管理番号であるSTAGEを使わない
+ *
+ * VPC構成はめったに更新せず、CDKによる既存VPCスタック更新は失敗率が高いので、
+ * VPC構成を変更する際には、都度新しいVPCを生成する。
+ * ただ、backend-app、backend-ecsと同じ頻度でブルーグリーンデプロイするのは無駄が多いので、
+ * これらとは別に世代管理をするために、本変数が必要である。
  */
-const generation = 2;
+const VPC_GENERATION = 2;
 
 dotenv.config();
 const service = process.env.SERVICE ?? "";
 const cidr = process.env.CIDR ?? "";
 const maxAzs = process.env.MAX_AZS ?? "";
+const account = process.env.CDK_DEFAULT_ACCOUNT ?? "";
+const region = process.env.CDK_DEFAULT_REGION ?? "";
 const parsedMaxAzs = Number.isInteger(maxAzs) ? Number.parseInt(maxAzs) : 1;
-const stackID = `${service}-vpc-g${generation}`;
+const stackID = `${service}-vpc-g${VPC_GENERATION}`;
 const app = new App();
-new AwsVpcStack(app, stackID, { cidr, maxAzs: parsedMaxAzs });
+new AwsVpcStack(app, stackID, { env: {account, region}, cidr, maxAzs: parsedMaxAzs });
