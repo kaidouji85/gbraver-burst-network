c09bbf0d7c2c2efab6a99afbcd94dedc3b9e55a8
kaidouji85 <kaidouji85@gmail.com>
2022-11-19 19:16:54 +0900

    VPCをマルチネットワーク対応させた

--- packages/backend-ecs/bin/backend-ecs.ts
+++ packages/backend-ecs/bin/backend-ecs.ts
@@ -3,21 +3,24 @@ import "source-map-support/register";
 
 import { App, Fn } from "aws-cdk-lib";
 import * as dotenv from "dotenv";
+import * as R from "ramda";
 import { v4 as uuidV4 } from "uuid";
 
 import { BackendEcsStack } from "../lib/backend-ecs-stack";
 
 dotenv.config();
-
+const VPC_GENERATION = 2;
 const service = process.env.SERVICE ?? "";
 const stage = process.env.STAGE ?? "dev";
 const matchMakeEcrRepositoryName =
   process.env.MATCH_MAKE_ECR_REPOSITORY_NAME ?? "";
-const vpcId = Fn.importValue(`${service}:VpcId`);
-const publicSubnetAvailabilityZone = Fn.importValue(
-  `${service}:PulicNetAvailabilityZone`
-);
-const publicSubnetId = Fn.importValue(`${service}:PulicSubnetId`);
+const vpcStackId = `${service}-g${VPC_GENERATION}`;
+const vpcId = Fn.importValue(`${vpcStackId}:VpcId`);
+const subnetCount = Number.parseInt(Fn.importValue(`${vpcStackId}:SubnetCount`));
+const subnetAzs: string[] = R.times(R.identity, subnetCount)
+    .map(index => Fn.importValue(`${vpcStackId}:PublicNetAvailabilityZone${index}`));
+const publicSubnetIds: string[] = R.times(R.identity, subnetCount)
+    .map(index => Fn.importValue(`${vpcStackId}:PublicSubnetId${index}`));
 const websocketAPIID = Fn.importValue(`${service}:${stage}:WebsoketApiId`);
 const connectionsTableARN = Fn.importValue(
   `${service}:${stage}:ConnectionsTableArn`
@@ -32,10 +35,10 @@ const app = new App();
 new BackendEcsStack(app, `${service}-${stage}-backend-ecs`, {
   service,
   stage,
-  vpcId,
-  publicSubnetAvailabilityZone,
-  publicSubnetId,
   websocketAPIID,
+  vpcId,
+  subnetAzs,
+  publicSubnetIds,
   connectionsTableARN,
   casualMatchEntriesTableARN,
   battlesTableARN,
