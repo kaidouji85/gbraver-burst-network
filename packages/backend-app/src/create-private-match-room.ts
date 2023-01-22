5800f086cf4d45b55522317bb8f4a1d87dda17c9
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-22 22:17:11 +0900

    リクエストボディパースを追加

--- packages/backend-app/src/create-private-match-room.ts
+++ packages/backend-app/src/create-private-match-room.ts
@@ -2,8 +2,11 @@ import { createAPIGatewayEndpoint } from "./api-gateway/endpoint";
 import { createApiGatewayManagementApi } from "./api-gateway/management";
 import { Notifier } from "./api-gateway/notifier";
 import { generatePrivateMatchRoomID } from "./core/generate-private-match-room-id";
+import { parseJSON } from "./json/parse";
 import { WebsocketAPIEvent } from "./lambda/websocket-api-event";
 import { WebsocketAPIResponse } from "./lambda/websocket-api-response";
+import { parseCreatePrivateMatchRoom } from "./request/create-private-match-room";
+import { Error } from "./response/websocket-response";
 
 const AWS_REGION = process.env.AWS_REGION ?? "";
 const STAGE = process.env.STAGE ?? "";
@@ -16,6 +19,11 @@ const apiGatewayEndpoint = createAPIGatewayEndpoint(
 const apiGateway = createApiGatewayManagementApi(apiGatewayEndpoint);
 const notifier = new Notifier(apiGateway);
 
+const invalidRequestBodyError: Error = {
+  action: "error",
+  error: "invalid request body",
+};
+
 /**
  * Websocket API createPrivateMatchRoom エントリポイント
  * @param event イベント
@@ -24,6 +32,19 @@ const notifier = new Notifier(apiGateway);
 export async function createPrivateMatchRoom(
   event: WebsocketAPIEvent
 ): Promise<WebsocketAPIResponse> {
+  const body = parseJSON(event.body);
+  const data = parseCreatePrivateMatchRoom(body);
+  if (!data) {
+    await notifier.notifyToClient(
+      event.requestContext.connectionId,
+      invalidRequestBodyError
+    );
+    return {
+      statusCode: 400,
+      body: "invalid request body",
+    };
+  }
+
   await notifier.notifyToClient(event.requestContext.connectionId, {
     action: "created-private-match-room",
     roomID: generatePrivateMatchRoomID(),
