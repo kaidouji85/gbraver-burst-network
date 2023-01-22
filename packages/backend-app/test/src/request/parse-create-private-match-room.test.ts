128e2d7263ef3d4fe8d41d86cf46ff2fdd2ae684
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-22 22:02:00 +0900

    テストケース追加

--- packages/backend-app/test/src/request/parse-create-private-match-room.test.ts
+++ packages/backend-app/test/src/request/parse-create-private-match-room.test.ts
@@ -14,6 +14,15 @@ test("CreatePrivateMatchRoomならパースできる", () => {
   expect(parseCreatePrivateMatchRoom(data)).toEqual(data);
 });
 
+test("データ型が異なるとパースできない", () => {
+  const data = {
+    action: "create-private-match-room",
+    armdozerId: 100,
+    pilotId: 100,
+  };
+  expect(parseCreatePrivateMatchRoom(data)).toEqual(null);
+});
+
 test("nullの場合、パースできない", () => {
   expect(parseCreatePrivateMatchRoom(null)).toBe(null);
 });
