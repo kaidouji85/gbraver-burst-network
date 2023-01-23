31b3ed67c203f718b5b60bb713cd4e682eb1890d
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-23 11:18:04 +0000

    private-match-entries DAO追加

---
+++ packages/backend-app/src/dynamo-db/private-match-entries.ts
@@ -0,0 +1,43 @@
+import { DynamoDB } from "aws-sdk";
+
+import { PrivateMatchEntry } from "../core/private-match-entry";
+
+/**
+ * private-match-entries スキーマ
+ * パーティションキー roomID
+ * ソートキー userID
+ */
+export type PrivateMatchEntriesSchema = PrivateMatchEntry;
+
+/** private-match-entries DAO */
+export class PrivateMatchEntries {
+  /** DynamoDBクライアント */
+  #client: DynamoDB.DocumentClient;
+  /** テーブル名 */
+  #tableName: string;
+
+  /**
+   * コンストラクタ
+   *
+   * @param client DynamoDBクライアント
+   * @param tableName テーブル名
+   */
+  constructor(client: DynamoDB.DocumentClient, tableName: string) {
+    this.#client = client;
+    this.#tableName = tableName;
+  }
+
+  /**
+   * 項目追加する
+   * @param command 追加する項目
+   * @return 処理が完了したら発火するPromise
+   */
+  async put(entry: PrivateMatchEntriesSchema): Promise<void> {
+    await this.#client
+      .put({
+        TableName: this.#tableName,
+        Item: entry,
+      })
+      .promise();
+  }
+}
