3ea619396070b22c42ba8996f70e0dee6a28c4ff
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-25 22:32:56 +0900

    parsePrivateMatchMakePolling実装

---
+++ packages/backend-app/src/request/private-match-make-polling.ts
@@ -0,0 +1,32 @@
+import { PrivateMatchRoomID } from "../core/private-match-room";
+
+/** プライベートルームマッチポーリング */
+export type PrivateMatchMakePolling = {
+  action: "private-match-make-polling";
+
+  /** ルームID */
+  roomID: PrivateMatchRoomID;
+};
+
+/**
+ * PrivateMatchMakePollingにパースする
+ * パースできない場合、nullを返す
+ * @param origin パース元
+ * @return パース結果
+ */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function parsePrivateMatchMakePolling(
+  origin: any
+): PrivateMatchMakePolling | null {
+  /* eslint-enable */
+  if (
+    origin?.action === "private-match-make-polling" &&
+    typeof origin?.roomID === "number"
+  ) {
+    return {
+      action: "private-match-make-polling",
+      roomID: origin.roomID,
+    };
+  }
+  return null;
+}
