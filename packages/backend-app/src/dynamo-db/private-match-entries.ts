5edeb39d4d497a61a38d56dfa1980641adb016ab
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-25 09:49:11 +0000

    エントリ検索追加

--- packages/backend-app/src/dynamo-db/private-match-entries.ts
+++ packages/backend-app/src/dynamo-db/private-match-entries.ts
@@ -1,6 +1,8 @@
 import { DynamoDB } from "aws-sdk";
 
 import { PrivateMatchEntry } from "../core/private-match-entry";
+import { PrivateMatchRoomID } from "../core/private-match-room";
+import { UserID } from "../core/user";
 
 /**
  * private-match-entries スキーマ
@@ -25,6 +27,28 @@ export class PrivateMatchEntries {
   constructor(client: DynamoDB.DocumentClient, tableName: string) {
     this.#client = client;
     this.#tableName = tableName;
+  }
+
+  /**
+   * エントリを検索する
+   * @param roomID ルームID
+   * @param userID ユーザID
+   * @return 検索結果、見つからない場合はnullを返す
+   */
+  async get(
+    roomID: PrivateMatchRoomID,
+    userID: UserID
+  ): Promise<PrivateMatchEntriesSchema | null> {
+    const result = await this.#client
+      .get({
+        TableName: this.#tableName,
+        Key: {
+          roomID,
+          userID,
+        },
+      })
+      .promise();
+    return result.Item ? (result.Item as PrivateMatchEntriesSchema) : null;
   }
 
   /**
