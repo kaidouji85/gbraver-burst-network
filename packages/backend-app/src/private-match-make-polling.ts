05f8a508b10a4164df5d34d6aa38b74fe2a29195
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-26 22:58:03 +0900

    バトル開始処理追加

--- packages/backend-app/src/private-match-make-polling.ts
+++ packages/backend-app/src/private-match-make-polling.ts
@@ -1,8 +1,15 @@
 import { createAPIGatewayEndpoint } from "./api-gateway/endpoint";
 import { createApiGatewayManagementApi } from "./api-gateway/management";
 import { Notifier } from "./api-gateway/notifier";
+import { BattlePlayer } from "./core/battle";
+import { InBattle } from "./core/connection";
+import { createBattle } from "./core/create-battle";
+import { createBattlePlayer } from "./core/create-battle-player";
 import { isValidPrivateMatch } from "./core/is-valid-private-match";
+import { privateMatchMake } from "./core/private-match-make";
 import { createDynamoDBClient } from "./dynamo-db/client";
+import { createBattles } from "./dynamo-db/create-battles";
+import { createConnections } from "./dynamo-db/create-connections";
 import { createPrivateMatchEntries } from "./dynamo-db/create-private-match-entries";
 import { createPrivateMatchRooms } from "./dynamo-db/create-private-match-rooms";
 import { parseJSON } from "./json/parse";
@@ -10,6 +17,7 @@ import { extractUserFromWebSocketAuthorizer } from "./lambda/extract-user";
 import { WebsocketAPIEvent } from "./lambda/websocket-api-event";
 import { WebsocketAPIResponse } from "./lambda/websocket-api-response";
 import { parsePrivateMatchMakePolling } from "./request/private-match-make-polling";
+import { createBattleStart } from "./response/create-battle-start";
 import type {
   CouldNotPrivateMatchMaking,
   Error,
@@ -23,6 +31,8 @@ const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
 const dynamoDB = createDynamoDBClient(AWS_REGION);
 const privateMatchRooms = createPrivateMatchRooms(dynamoDB, SERVICE, STAGE);
 const privateMatchEntries = createPrivateMatchEntries(dynamoDB, SERVICE, STAGE);
+const battles = createBattles(dynamoDB, SERVICE, STAGE);
+const connections = createConnections(dynamoDB, SERVICE, STAGE);
 
 const apiGatewayEndpoint = createAPIGatewayEndpoint(
   WEBSOCKET_API_ID,
@@ -89,6 +99,43 @@ export async function privateMatchMakePolling(
     );
     return endPrivateMatchMakePolling;
   }
+
+  const matching = privateMatchMake(room, entries);
+  if (!matching) {
+    await notifier.notifyToClient(
+      event.requestContext.connectionId,
+      cloudNotPrivateMatchMake
+    );
+    return endPrivateMatchMakePolling;
+  }
+
+  const players: [BattlePlayer, BattlePlayer] = [
+    createBattlePlayer(matching[0]),
+    createBattlePlayer(matching[1]),
+  ];
+  const battle = createBattle(players);
+  const updatedConnectionState: InBattle = {
+    type: "InBattle",
+    battleID: battle.battleID,
+    players,
+  };
+  const updatedConnections = matching.map((v) => ({
+    connectionId: v.connectionId,
+    userID: v.userID,
+    state: updatedConnectionState,
+  }));
+  const notices = matching.map((entry) => {
+    const data = createBattleStart(entry.userID, battle);
+    return {
+      connectionId: entry.connectionId,
+      data,
+    };
+  });
+  await Promise.all([
+    battles.put(battle),
+    ...updatedConnections.map((v) => connections.put(v)),
+    ...notices.map((v) => notifier.notifyToClient(v.connectionId, v.data)),
+  ]);
 
   return endPrivateMatchMakePolling;
 }
