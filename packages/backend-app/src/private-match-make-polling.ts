4091e8350384d3a42c3f78388b2c606e572edaa2
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-26 02:52:12 +0000

    ユーザ情報取得処理まで書いた

--- packages/backend-app/src/private-match-make-polling.ts
+++ packages/backend-app/src/private-match-make-polling.ts
@@ -1,15 +1,22 @@
 import { createAPIGatewayEndpoint } from "./api-gateway/endpoint";
 import { createApiGatewayManagementApi } from "./api-gateway/management";
 import { Notifier } from "./api-gateway/notifier";
+import { createDynamoDBClient } from "./dynamo-db/client";
+import { createPrivateMatchRooms } from "./dynamo-db/create-private-match-rooms";
 import { parseJSON } from "./json/parse";
+import { extractUserFromWebSocketAuthorizer } from "./lambda/extract-user";
 import { WebsocketAPIEvent } from "./lambda/websocket-api-event";
 import { WebsocketAPIResponse } from "./lambda/websocket-api-response";
 import { parsePrivateMatchMakePolling } from "./request/private-match-make-polling";
-import type { Error } from "./response/websocket-response";
+import type { CouldNotPrivateMatchMaking, Error } from "./response/websocket-response";
 
 const AWS_REGION = process.env.AWS_REGION ?? "";
+const SERVICE = process.env.SERVICE ?? "";
 const STAGE = process.env.STAGE ?? "";
 const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
+
+const dynamoDB = createDynamoDBClient(AWS_REGION);
+const privateMatchRooms = createPrivateMatchRooms(dynamoDB, SERVICE, STAGE);
 
 const apiGatewayEndpoint = createAPIGatewayEndpoint(
   WEBSOCKET_API_ID,
@@ -22,6 +29,9 @@ const notifier = new Notifier(apiGateway);
 const invalidRequestBodyError: Error = {
   action: "error",
   error: "invalid request body",
+};
+const cloudNotPrivateMatchMake: CouldNotPrivateMatchMaking = {
+  action: "cloud-not-private-match-making",
 };
 
 /**
@@ -44,6 +54,11 @@ export async function privateMatchMakePolling(
       body: "invalid request body",
     };
   }
+
+  const user = extractUserFromWebSocketAuthorizer(
+    event.requestContext.authorizer
+  );
+  
   return {
     statusCode: 200,
     body: "end private match make polling",
