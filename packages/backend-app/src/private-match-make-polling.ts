96cb0cd9115044bb01968d93c01757ddb507f1db
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-26 02:34:58 +0000

    リクエストボディチェック追加

--- packages/backend-app/src/private-match-make-polling.ts
+++ packages/backend-app/src/private-match-make-polling.ts
@@ -1,7 +1,28 @@
+import { createAPIGatewayEndpoint } from "./api-gateway/endpoint";
+import { createApiGatewayManagementApi } from "./api-gateway/management";
+import { Notifier } from "./api-gateway/notifier";
 import { parseJSON } from "./json/parse";
 import { WebsocketAPIEvent } from "./lambda/websocket-api-event";
 import { WebsocketAPIResponse } from "./lambda/websocket-api-response";
 import { parsePrivateMatchMakePolling } from "./request/private-match-make-polling";
+import type { Error } from "./response/websocket-response";
+
+const AWS_REGION = process.env.AWS_REGION ?? "";
+const STAGE = process.env.STAGE ?? "";
+const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
+
+const apiGatewayEndpoint = createAPIGatewayEndpoint(
+  WEBSOCKET_API_ID,
+  AWS_REGION,
+  STAGE
+);
+const apiGateway = createApiGatewayManagementApi(apiGatewayEndpoint);
+const notifier = new Notifier(apiGateway);
+
+const invalidRequestBodyError: Error = {
+  action: "error",
+  error: "invalid request body",
+};
 
 /**
  * プライベートマッチメイクポーリング
@@ -14,7 +35,10 @@ export async function privateMatchMakePolling(
   const body = parseJSON(event.body);
   const data = parsePrivateMatchMakePolling(body);
   if (!data) {
-    
+    await notifier.notifyToClient(
+      event.requestContext.connectionId,
+      invalidRequestBodyError
+    );
     return {
       statusCode: 400,
       body: "invalid request body",
