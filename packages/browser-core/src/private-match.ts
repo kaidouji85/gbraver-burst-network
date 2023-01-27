1d223a31f927edeb5aa1a64526efd3d6c7b67f36
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-27 09:43:51 +0000

    PrivateMatchRoom追加

--- packages/browser-core/src/private-match.ts
+++ packages/browser-core/src/private-match.ts
@@ -1,10 +1,9 @@
 import { ArmDozerId, PilotId } from "gbraver-burst-core";
 
-/** プライベートマッチルームID */
-export type PrivateMatchRoomID = string;
+import { Battle } from "./battle";
 
 /** プライベートマッチルーム生成 */
-export interface PrivateMatchCreator {
+export interface PrivateMatchCreate {
   /**
    * プライベートマッチルームを生成する
    * @param armdozerId 選択したアームドーザのID
@@ -14,5 +13,20 @@ export interface PrivateMatchCreator {
   createPrivateMatchRoom(
     armdozerId: ArmDozerId,
     pilotId: PilotId
-  ): Promise<PrivateMatchRoomID>;
+  ): Promise<PrivateMatchRoom>;
+}
+
+/** プライベートマッチルームID */
+export type PrivateMatchRoomID = string;
+
+/** プライベートマッチルーム */
+export interface PrivateMatchRoom {
+  /** ルームID、本プロパティを招待するユーザに共有する */
+  roomID: PrivateMatchRoomID;
+
+  /**
+   * マッチングが成立するまで待機する
+   * @return バトル
+   */
+  waitUntilMatching(): Promise<Battle>;
 }
