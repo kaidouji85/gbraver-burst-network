1380ee4305419c3870a5fd0dcda583d25e5347dd
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-28 14:16:46 +0900

    プライベートマッチルーム作成ユースケース追加

---
+++ packages/serverless-stub/src/use-case/private-match-room-owner.ts
@@ -0,0 +1,31 @@
+import { BrowserSDK } from "@gbraver-burst-network/browser-sdk";
+import { ArmDozerIds, PilotIds } from "gbraver-burst-core";
+
+import { UseCase } from "./use-case";
+
+/** プライベートマッチオーナー */
+export class PrivateMatchRoomOwner implements UseCase {
+  #sdk: BrowserSDK;
+
+  /**
+   * コンストラクタ
+   * @param sdk ブラウザSDK
+   */
+  constructor(sdk: Readonly<BrowserSDK>) {
+    this.#sdk = sdk;
+  }
+
+  /** @override */
+  name(): string {
+    return "プライベートマッチ　オーナー";
+  }
+
+  /** @override */
+  async execute(): Promise<void> {
+    const room = await this.#sdk.createPrivateMatchRoom(
+      ArmDozerIds.SHIN_BRAVER,
+      PilotIds.SHINYA
+    );
+    console.log(room.roomID);
+  }
+}
