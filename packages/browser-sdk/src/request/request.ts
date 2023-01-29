b78d8d21388e802a1e5400f6d5c74be7c5a0e3e0
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-29 16:41:29 +0900

    APIServerRequestにAPIServerRequestを追加

--- packages/browser-sdk/src/request/request.ts
+++ packages/browser-sdk/src/request/request.ts
@@ -1,6 +1,7 @@
 import type { BattleProgressPolling } from "./battle-progress-polling";
 import { CreatePrivateMatchRoom } from "./create-private-match-room";
 import type { EnterCasualMatch } from "./enter-casual-match";
+import { EnterPrivateMatchRoom } from "./enter-private-match-room";
 import type { Ping } from "./ping";
 import type { SendCommand } from "./send-command";
 
@@ -10,4 +11,5 @@ export type APIServerRequest =
   | EnterCasualMatch
   | SendCommand
   | BattleProgressPolling
-  | CreatePrivateMatchRoom;
+  | CreatePrivateMatchRoom
+  | EnterPrivateMatchRoom;
