d120c0902ec117219bf19e5e777a8b07aa55debe
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-01 00:58:29 +0000

    プライベートマッチを一通り実行するユースケースを追加

--- packages/serverless-stub/src/use-case/private-match-room-player.ts
+++ packages/serverless-stub/src/use-case/private-match-room-player.ts
@@ -32,5 +32,35 @@ export class PrivateMatchRoomPlayer implements UseCase {
       console.log("cloud not matching");
       return;
     }
+
+    battle.suddenlyBattleNotifier().subscribe(() => {
+      console.log("suddenly battle end");
+    });
+    console.log(battle.player, battle.enemy, battle.initialState);
+    const update01 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 3,
+    });
+    console.log(update01);
+    const update02 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update02);
+    const update03 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update03);
+    const update04 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 0,
+    });
+    console.log(update04);
+    const update05 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 0,
+    });
+    console.log(update05);
   }
 }
