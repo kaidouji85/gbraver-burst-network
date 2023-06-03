6e545d254b6cf32788d6b08ad3f701165ee0fb49
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-03 14:43:04 +0900

    hotswapデプロイフラグを追加

--- backend-ecs-deploy.sh
+++ backend-ecs-deploy.sh
@@ -3,11 +3,12 @@
 if [ -z "$STAGE" ] ||
   [ -z "$MATCH_MAKE_ECR_REPOSITORY_NAME" ] ||
   [ -z "$VPC_SUBNET_COUNT" ] ||
-  [ -z "$DOCKER_IMAGE_TAG" ]; then
+  [ -z "$DOCKER_IMAGE_TAG" ] ||
+  [ -z "$SHOULD_ECS_HOTSWAP" ]; then
   echo 'required environment variables are not defined'
   exit 1
 fi
 
 OWN_PATH=$(cd "$(dirname "${0}")" && pwd)
 cd "${OWN_PATH}/packages/backend-ecs" || exit
-npx cdk deploy -f --require-approval never
+npx cdk deploy -f --require-approval never --hotswap="${SHOULD_ECS_HOTSWAP}"
