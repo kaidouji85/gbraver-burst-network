329586e8e344ff2be0292f267c7a790ebde99905
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-09 05:50:15 +0000

    ユニットテスト追加

---
+++ packages/backend-app/test/src/request/parse-ping.test.ts
@@ -0,0 +1,22 @@
+import { parsePing, Ping } from "../../../src/request/ping";
+
+/** 有効なping */
+const ping: Ping = {
+  action: "ping"
+};
+
+test("pingはパースできる", () => {
+  expect(parsePing(ping)).toEqual(ping);
+});
+
+test("余計なプロパティは削除してパースする", () => {
+  expect(parsePing({ ...ping, hp: 1000 })).toEqual(ping);
+});
+
+test("nullはパースできない", () => {
+  expect(parsePing(null)).toBe(null);
+});
+
+test("undefinedはパースできない", () => {
+  expect(parsePing(undefined)).toBe(null);
+});
