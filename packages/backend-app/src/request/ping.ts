0a89d25ac5dda4db766a1dbc39eb23aed4d5b43b
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-09 05:45:39 +0000

    Pingをzod化

--- packages/backend-app/src/request/ping.ts
+++ packages/backend-app/src/request/ping.ts
@@ -1,4 +1,24 @@
+import { z } from "zod";
+
 /** ping */
 export type Ping = {
   action: "ping";
 };
+
+/** ping zodスキーマ */
+export const PingSchema = z.object({
+  action: z.literal("ping"),
+});
+
+/**
+ * 任意のオブジェクトをPingにパースする
+ * パースできない場合はnullを返す
+ * @param origin パース元
+ * @return パース結果
+ */
+export function parsePing(
+  origin: unknown
+): Ping | null {
+  const result = PingSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
