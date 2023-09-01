3d2b0c61c5928a31ae48f785b59d612d34877390
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-01 07:25:51 +0000

    EnteredCasualMatchをzod化

--- packages/browser-sdk/src/response/entered-casual-match.ts
+++ packages/browser-sdk/src/response/entered-casual-match.ts
@@ -1,21 +1,22 @@
+import { z } from "zod";
+
 /** カジュアルマッチ入室成功 */
 export type EnteredCasualMatch = {
   action: "entered-casual-match";
 };
 
+/** EnteredCasualMatch zodスキーマ */
+export const EnteredCasualMatchSchema = z.object({
+  action: z.literal("entered-casual-match"),
+});
+
 /**
  * 任意のオブジェクトをカジュアルマッチ入室成功にパースする
  * パースできない場合はnullを返す
- *
  * @param data パース元となるオブジェクト
  * @return パース結果
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export function parseEnteredCasualMatch(data: any): EnteredCasualMatch | null {
-  /* eslint-enable */
-  return data?.action === "entered-casual-match"
-    ? {
-        action: data.action,
-      }
-    : null;
+export function parseEnteredCasualMatch(data: unknown): EnteredCasualMatch | null {
+  const result = EnteredCasualMatchSchema.safeParse(data);
+  return result.success ? result.data : null;
 }
