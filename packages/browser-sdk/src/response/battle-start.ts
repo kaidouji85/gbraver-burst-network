d9331629db2888c877d1d273964772fe5e68636e
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-01 07:22:50 +0000

    BattleStartをzod化

--- packages/browser-sdk/src/response/battle-start.ts
+++ packages/browser-sdk/src/response/battle-start.ts
@@ -1,56 +1,41 @@
-import type { GameState, Player } from "gbraver-burst-core";
+import { GameState, GameStateSchema, Player, PlayerSchema } from "gbraver-burst-core";
+import { z } from "zod";
 
 /** バトルスタート */
 export type BattleStart = {
   action: "battle-start";
-
   /** プレイヤー情報 */
   player: Player;
-
   /** 敵情報 */
   enemy: Player;
-
   /** ステートヒストリー */
   stateHistory: GameState[];
-
   /** 戦闘ID */
   battleID: string;
-
   /** フローID */
   flowID: string;
-
   /** 戦闘進捗ポーリングを実行する側か否か、trueでポーリングをする */
   isPoller: boolean;
 };
 
+/** BattleStart zodスキーマ */
+export const BattleStartSchema = z.object({
+  action: z.literal("battle-start"),
+  player: PlayerSchema,
+  enemy: PlayerSchema,
+  stateHistory: z.array(GameStateSchema),
+  battleID: z.string(),
+  flowID: z.string(),
+  isPoller: z.boolean(),
+});
+
 /**
  * 任意のオブジェクトをStartBattleにパースする
  * パースできない場合はnullを返す
- *
  * @param data パース元となる文字列
  * @return パース結果
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export function parseBattleStart(data: any): BattleStart | null {
-  /* eslint-enable */
-  // TODO player、enemy、stateHistoryの正確な型チェックを実装する
-  return data?.action === "battle-start" &&
-    typeof data?.battleID === "string" &&
-    typeof data?.flowID === "string" &&
-    Array.isArray(data?.stateHistory) &&
-    data?.player !== null &&
-    typeof data?.player === "object" &&
-    data?.enemy !== null &&
-    typeof data?.enemy === "object" &&
-    typeof data?.isPoller === "boolean"
-    ? {
-        action: data.action,
-        battleID: data.battleID,
-        flowID: data.flowID,
-        stateHistory: data.stateHistory,
-        player: data.player,
-        enemy: data.enemy,
-        isPoller: data.isPoller,
-      }
-    : null;
+export function parseBattleStart(data: unknown): BattleStart | null {
+  const result = BattleStartSchema.safeParse(data);
+  return result.success ? result.data : null;
 }
