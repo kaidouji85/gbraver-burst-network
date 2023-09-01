ac3334de7f7b839927e50611026e798c74b51422
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-01 07:19:35 +0000

    NotReadyBattleProgressをzod化

--- packages/browser-sdk/src/response/not-ready-battle-progress.ts
+++ packages/browser-sdk/src/response/not-ready-battle-progress.ts
@@ -1,23 +1,24 @@
+import { z } from "zod"
+
 /** バトル進行の準備ができていない */
 export type NotReadyBattleProgress = {
   action: "not-ready-battle-progress";
 };
 
+/** NotReadyBattleProgress zodスキーマ */
+export const NotReadyBattleProgressSchema = z.object({
+  action: z.literal("not-ready-battle-progress"),
+});
+
 /**
  * 任意オブジェクトをNotReadyBattleProgressにパースする
  * パースできない場合はnullを返す
- *
  * @param data パース元オブジェクト
  * @return パース結果
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export function parseNotReadyBattleProgress(
-  data: any,
+  data: unknown,
 ): NotReadyBattleProgress | null {
-  /* eslint-enable */
-  return data?.action === "not-ready-battle-progress"
-    ? {
-        action: data.action,
-      }
-    : null;
+  const result = NotReadyBattleProgressSchema.safeParse(data);
+  return result.success ? result.data : null;
 }
