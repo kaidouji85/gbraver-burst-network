0b8b4a441c3090c42bbb680f04b6bb3302a5478a
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-06 02:56:56 +0000

    SuddenlyBattleEndをzod化

--- packages/browser-sdk/src/response/suddenly-battle-end.ts
+++ packages/browser-sdk/src/response/suddenly-battle-end.ts
@@ -1,21 +1,22 @@
+import { z } from "zod";
+
 /** バトル強制終了 */
 export type SuddenlyBattleEnd = {
   action: "suddenly-battle-end";
 };
 
+/** SuddenlyBattleEnd zodスキーマ */
+export const SuddenlyBattleEndSchema = z.object({
+  action: z.literal("suddenly-battle-end"),
+});
+
 /**
  * 任意オブジェクトをSuddenlyBattleEndにパースする
  * パースできない場合はnullを返す
- *
  * @param data パース元オブジェクト
  * @return パース結果
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export function parseSuddenlyBattleEnd(data: any): SuddenlyBattleEnd | null {
-  /* eslint-enable */
-  return data?.action === "suddenly-battle-end"
-    ? {
-        action: data.action,
-      }
-    : null;
+export function parseSuddenlyBattleEnd(data: unknown): SuddenlyBattleEnd | null {
+  const result = SuddenlyBattleEndSchema.safeParse(data);
+  return result.success ? result.data : null;
 }
