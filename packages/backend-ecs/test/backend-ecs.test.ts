3d0819c372ed54d99356fbdffd793c680df74d0f
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-16 09:33:00 +0000

    backend-ecsにcode-format-checkを追加した

--- packages/backend-ecs/test/backend-ecs.test.ts
+++ packages/backend-ecs/test/backend-ecs.test.ts
@@ -1,23 +1,23 @@
-import {App} from 'aws-cdk-lib';
-import {Template} from "aws-cdk-lib/assertions";
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
 
-import {BackendEcsStack} from '../lib/backend-ecs-stack';
+import { BackendEcsStack } from "../lib/backend-ecs-stack";
 
-test('バックエンドECSスタックがスナップショットと一致している', () => {
-    const app = new App();
-    const stack = new BackendEcsStack(app, 'MyTestStack', {
-      service: 'gbraver-buesr-sls-dev',
-      stage: 'v1-5-1',
-      vpcId: 'MyVpcId',
-      subnetAzs: ['az-a', 'az-b', 'az-c'],
-      publicSubnetIds: ['subnet-id-0', 'subnet-id-1', 'subnet-id-2'],
-      websocketAPIID: 'MyWebsocketAPIID',
-      connectionsTableARN: 'MyConnectionsTableARN',
-      casualMatchEntriesTableARN: 'MyCasualMatchEntriesTableARN',
-      battlesTableARN: 'MyBattlesTableARN',
-      matchMakeEcrRepositoryName: 'MyMatchMakeRepositoryName',
-      dockerImageTag: 'MyDockerImageTag',
-    });
-    const template = Template.fromStack(stack).toJSON();
-    expect(template).toMatchSnapshot();
+test("バックエンドECSスタックがスナップショットと一致している", () => {
+  const app = new App();
+  const stack = new BackendEcsStack(app, "MyTestStack", {
+    service: "gbraver-buesr-sls-dev",
+    stage: "v1-5-1",
+    vpcId: "MyVpcId",
+    subnetAzs: ["az-a", "az-b", "az-c"],
+    publicSubnetIds: ["subnet-id-0", "subnet-id-1", "subnet-id-2"],
+    websocketAPIID: "MyWebsocketAPIID",
+    connectionsTableARN: "MyConnectionsTableARN",
+    casualMatchEntriesTableARN: "MyCasualMatchEntriesTableARN",
+    battlesTableARN: "MyBattlesTableARN",
+    matchMakeEcrRepositoryName: "MyMatchMakeRepositoryName",
+    dockerImageTag: "MyDockerImageTag",
+  });
+  const template = Template.fromStack(stack).toJSON();
+  expect(template).toMatchSnapshot();
 });
