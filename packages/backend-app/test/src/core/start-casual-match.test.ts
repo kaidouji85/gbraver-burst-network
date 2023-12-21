75fe84f26146edb2650aaae7c8faf8ebe9146398
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-21 02:28:13 +0000

    startCasualMatchのユニットテストを追加

---
+++ packages/backend-app/test/src/core/start-casual-match.test.ts
@@ -0,0 +1,29 @@
+import { ArmdozerIds, PilotIds } from "gbraver-burst-core";
+import { v4 as uuidv4 } from "uuid";
+
+import { CasualMatchEntry } from "../../../src/core/casual-match-entry";
+import { startCasualMatch } from "../../../src/core/start-casual-match";
+import { mockUniqUUID } from "../../mock-unique-uuid";
+
+jest.mock('uuid');
+
+/** カジュアルマッチ参加プレイヤー1 */
+const entry01: CasualMatchEntry = {
+  userID: "user-01",
+  armdozerId: ArmdozerIds.SHIN_BRAVER,
+  pilotId: PilotIds.SHINYA,
+  connectionId: "user-01-connection-id",
+};
+
+/** カジュアルマッチ参加プレイヤー2 */
+const entry02: CasualMatchEntry = {
+  userID: "user-02",
+  armdozerId: ArmdozerIds.NEO_LANDOZER,
+  pilotId: PilotIds.GAI,
+  connectionId: "user-02-connection-id",
+};
+
+test("カジュアルマッチを正しく開始できる", () => {
+  (uuidv4 as jest.Mock).mockImplementation(mockUniqUUID());
+  expect(startCasualMatch([entry01, entry02])).toMatchSnapshot();
+});
