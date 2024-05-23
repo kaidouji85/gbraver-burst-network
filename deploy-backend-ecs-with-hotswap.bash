51350c32b1d479697c816289b185adab9d9205bd
kaidouji85 <kaidouji85@gmail.com>
2024-05-23 10:35:09 +0000

    シェルチェックのルールを厳しくした

--- deploy-backend-ecs-with-hotswap.bash
+++ deploy-backend-ecs-with-hotswap.bash
@@ -3,4 +3,4 @@ set -Ceu
 
 OWN_PATH=$(cd "$(dirname "${0}")" && pwd)
 cd "${OWN_PATH}/packages/backend-ecs" || exit
-npx cdk deploy -f --require-approval never --hotswap
+npx cdk deploy --require-approval never --hotswap
--- push-match-make-container.bash
+++ push-match-make-container.bash
@@ -2,12 +2,15 @@
 set -Ceu
 
 OWN_PATH=$(cd "$(dirname "${0}")" && pwd)
-cd "${OWN_PATH}/packages/backend-app" || exit
+cd "${OWN_PATH}/packages/backend-app" || exit 1
 npm run build:match-make
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-aws ecr get-login-password --region "$AWS_DEFAULT_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USER" --password-stdin
-IMAGE_REPO_NAME=$MATCH_MAKE_ECR_REPOSITORY_NAME
-docker build -t "${IMAGE_REPO_NAME}:${DOCKER_IMAGE_TAG}" -f matchMake.Dockerfile .
+# aws ecr get-login-passwordに失敗しても甚大な影響はないので、以下のshellcheckは無視する
+# shellcheck disable=SC2312
+aws ecr get-login-password --region "${AWS_DEFAULT_REGION:?}" |
+  docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+echo "${DOCKER_TOKEN:?}" | docker login -u "${DOCKER_USER:?}" --password-stdin
+IMAGE_REPO_NAME=${MATCH_MAKE_ECR_REPOSITORY_NAME:?}
+docker build -t "${IMAGE_REPO_NAME}:${DOCKER_IMAGE_TAG:?}" -f matchMake.Dockerfile .
 docker tag "${IMAGE_REPO_NAME}:${DOCKER_IMAGE_TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${DOCKER_IMAGE_TAG}"
-docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/$IMAGE_REPO_NAME:$DOCKER_IMAGE_TAG"
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${DOCKER_IMAGE_TAG}"
