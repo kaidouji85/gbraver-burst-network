f09feb0c71fdfc4866d293134a3766f9949eafd0
kaidouji85 <kaidouji85@gmail.com>
2025-03-06 07:26:07 +0000

    backend-appのprettierを見直した

--- packages/backend-app/jest.config.js
+++ packages/backend-app/jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   collectCoverage: true,
   coverageDirectory: "coverage",
-  coverageReporters: [['lcov', { projectRoot: '../../' }]],
+  coverageReporters: [["lcov", { projectRoot: "../../" }]],
 };
