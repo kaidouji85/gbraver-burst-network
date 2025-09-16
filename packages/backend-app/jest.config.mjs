10d4ad5b1e0f872cc180f5d3ac0612917a6b1552
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-16 21:59:47 +0900

    jest.config.mjsのコメントを更新し、uuidパッケージの変換対象を明示化した

--- packages/backend-app/jest.config.mjs
+++ packages/backend-app/jest.config.mjs
@@ -5,14 +5,11 @@ export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: [["lcov", { projectRoot: "../../" }]],
-  // 変換を無視するパターン（マッチしたファイルは変換されない）
-  // 正規表現の否定先読みで、uuidパッケージを変換対象に含める
+  // 正規表現の否定先読みで、uuidパッケージを変換対象に含めている
   // 参考文献
   // https://qiita.com/konyaru/items/b22916308ee6f03272db
   transformIgnorePatterns: ["node_modules/(?!(uuid)/)"],
-  // uuidパッケージのES Modules対応
   transform: {
-    //"^.+\\.tsx?$": "ts-jest",
     "^.+\\.jsx?$": "babel-jest",
   },
 };
