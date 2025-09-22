5448525dcf67def7cd821e7737c0a116eaa706b3
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-22 09:23:26 +0900

    feat: add offline-browser-sdk package with initial configuration and files

---
+++ packages/offline-browser-sdk/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const BUILD_PATH = "build";
+const BUILD_INDEX_JS_PATH = `index.js`;
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    [BUILD_INDEX_JS_PATH]: path.resolve(__dirname, "src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, BUILD_PATH),
+    filename: "[name]",
+  },
+  devServer: {
+    static: path.resolve(__dirname, BUILD_PATH),
+    port: 8080,
+    host: "0.0.0.0",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, `${BUILD_PATH}/index.html`),
+      template: "src/index.html",
+      inject: true,
+    }),
+  ],
+};
