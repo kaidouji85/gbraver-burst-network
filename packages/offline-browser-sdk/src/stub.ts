08705bab97e922880491c47bada8b99714622378
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-23 13:02:27 +0900

    feat: implement armdozer and pilot selection options in the offline browser SDK

--- packages/offline-browser-sdk/src/stub.ts
+++ packages/offline-browser-sdk/src/stub.ts
@@ -1,12 +1,43 @@
-import { ArmdozerIds, PilotIds } from "gbraver-burst-core";
+import { ArmdozerIds, Armdozers, PilotIds, Pilots } from "gbraver-burst-core";
 
 import { createOfflineBrowserSDK } from "./";
 
 /** バックエンドURL */
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
 
+/**
+ * アームドーザの選択肢を更新する
+ */
+const updateArmdozerOptions = () => {
+  const armdozerSelect = document.getElementById(
+    "armdozer",
+  ) as HTMLSelectElement;
+  Armdozers.forEach((armdozer) => {
+    const option = document.createElement("option");
+    option.value = armdozer.id;
+    option.text = armdozer.name;
+    armdozerSelect.appendChild(option);
+  });
+};
+
+/**
+ * パイロットの選択肢を更新する
+ */
+const updatePilotOptions = () => {
+  const pilotSelect = document.getElementById("pilot") as HTMLSelectElement;
+  Pilots.forEach((pilot) => {
+    const option = document.createElement("option");
+    option.value = pilot.id;
+    option.text = pilot.name;
+    pilotSelect.appendChild(option);
+  });
+};
+
 /** スタブのエントリポイント */
 window.onload = () => {
+  updateArmdozerOptions();
+  updatePilotOptions();
+
   const sdk = createOfflineBrowserSDK({ backendURL: BACKEND_URL });
   sdk.enterRoom({
     armdozerId: ArmdozerIds.SHIN_BRAVER,
