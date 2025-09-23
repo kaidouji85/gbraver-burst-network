3aa34e581230e0081906acb427aa6c08eb2a0f3e
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-23 10:38:18 +0900

    feat: add dotenv support for environment variables and update CORS configuration

--- packages/offline-backend-app/src/index.ts
+++ packages/offline-backend-app/src/index.ts
@@ -1,31 +1,37 @@
-import { Server } from "socket.io";
-import express from "express";
 import { createServer } from "node:http";
+
 import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import { Server } from "socket.io";
+
+dotenv.config();
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:8080";
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:8080",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
-    credentials: true
-  }
+    origin: process.env.CORS_ORIGIN || "http://localhost:8080",
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
 
-const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
