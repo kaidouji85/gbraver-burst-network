b0f9015cf78a48688e8f0338e4e3575a779412de
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-27 10:36:49 +0900

    refactor: 不要なインポートを削除し、関数のコメントを更新

--- packages/offline-browser-sdk/src/stub.ts
+++ packages/offline-browser-sdk/src/stub.ts
@@ -1,13 +1,6 @@
-import {
-  Armdozers,
-  InputCommand,
-  Pilots,
-  Player,
-  Selectable,
-} from "gbraver-burst-core";
+import { Armdozers, Pilots, Player, Selectable } from "gbraver-burst-core";
 
 import { createOfflineBrowserSDK } from "./";
-import { BattleInfo } from "./offline-browser-sdk";
 
 /** バックエンドURL */
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
@@ -53,7 +46,7 @@ const updatePilotOptions = () => {
 };
 
 /**
- * セレクターを操作不可能にする
+ * キャラクターセレクターを操作不可能にする
  */
 const disabledSelector = () => {
   document.getElementById("armdozer")?.setAttribute("disabled", "true");
@@ -61,10 +54,16 @@ const disabledSelector = () => {
   document.getElementById("enter-room")?.setAttribute("disabled", "true");
 };
 
+/**
+ * コマンドセレクターを非表示にする
+ */
 const hiddenCommands = () => {
   document.getElementById("commands")?.setAttribute("style", "display:none");
 };
 
+/**
+ * コマンドセレクターを表示する
+ */
 const showCommands = () => {
   document.getElementById("commands")?.setAttribute("style", "display:block");
 };
@@ -77,13 +76,20 @@ const updateCommands = (selectable: Selectable) => {
   const commandSelector = document.getElementById(
     "command-selector",
   ) as HTMLSelectElement;
-  selectable.command.forEach((command, index) => {
+  selectable.command.forEach((command) => {
     const option = document.createElement("option");
-    option.value = index.toString();
-    option.text = JSON.stringify(command);
+    const jsonStr = JSON.stringify(command);
+    option.value = jsonStr;
+    option.text = jsonStr;
     commandSelector.appendChild(option);
   });
 };
+
+/**
+ * 現在のプレイヤー情報
+ * 対戦以外ではnullが入る
+ */
+let player: Player | null = null;
 
 /** スタブのエントリポイント */
 window.onload = () => {
@@ -92,6 +98,9 @@ window.onload = () => {
   updatePilotOptions();
   const sdk = createOfflineBrowserSDK({ backendURL: BACKEND_URL });
 
+  /**
+   * 入室ボタンのクリックイベント
+   */
   document.getElementById("enter-room")?.addEventListener("click", async () => {
     const armdozerSelect = getArmdozerSelector();
     const pilotSelect = getPilotSelector();
@@ -114,6 +123,7 @@ window.onload = () => {
       (p) => p.playerId === battleInfo.player.playerId,
     );
     if (commands?.selectable) {
+      player = battleInfo.player;
       updateCommands(commands);
       showCommands();
     }
