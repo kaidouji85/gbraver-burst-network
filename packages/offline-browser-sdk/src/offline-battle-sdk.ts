730669afb8c4e456d942cb0f87087cc5bf6de561
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-29 14:17:02 +0900

    feat: オフライン用バトルSDKのインターフェースと実装を追加

---
+++ packages/offline-browser-sdk/src/offline-battle-sdk.ts
@@ -0,0 +1,31 @@
+import { Command, GameState, Player } from "gbraver-burst-core";
+import { Observable } from "rxjs";
+
+/**
+ * オフライン用バトルSDK
+ * インターフェースはpackages/browser-sdk/src/browser-sdk/battle-sdk.tsのBattleSDKと同一
+ * 本操作はログイン後に実行することを想定している
+ */
+export interface OfflineBattleSDK {
+  /** プレイヤーの情報 */
+  player: Player;
+
+  /** 対戦相手の情報 */
+  enemy: Player;
+
+  /** ゲームの初期状態 */
+  initialState: GameState[];
+
+  /**
+   * バトルを進行させる
+   * @param command プレイヤーが入力するコマンド
+   * @returns ゲーム結果
+   */
+  progress(command: Command): Promise<GameState[]>;
+
+  /**
+   * バトル強制終了の通知ストリーム
+   * @returns 通知ストリーム
+   */
+  suddenlyBattleEndNotifier(): Observable<unknown>;
+}
