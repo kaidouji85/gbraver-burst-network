ce3f66bf0390f9f001c0a329db6e5114d3f08258
Y.Takeuchi <kaidouji85@gmail.com>
2025-11-24 11:38:27 +0900

    DynamoDBクライアントのデュアルスタックエンドポイント使用オプションを削除

--- packages/backend-app/src/dynamo-db/dynamo-db-document.ts
+++ packages/backend-app/src/dynamo-db/dynamo-db-document.ts
@@ -5,18 +5,13 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
  * DynamoDBクライアントを生成する
  *
  * @param region AWSのリージョン
- * @param useDualstackEndpoint AWS APIのデュアルスタックエンドポイントを使用するかどうか、falseであれば標準エンドポイントを使用する
  * @returns DynamoDBクライアント
  */
-export function createDynamoDBDocument(
-  region: string,
-  useDualstackEndpoint = false,
-): DynamoDBDocument {
+export function createDynamoDBDocument(region: string): DynamoDBDocument {
   return DynamoDBDocument.from(
     new DynamoDB({
       apiVersion: "2012-08-10",
       region,
-      useDualstackEndpoint,
     }),
   );
 }
