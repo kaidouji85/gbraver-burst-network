269169db1436f846783ff98e1659888fe01fbad5
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-06 12:31:46 +0900

    feat: remove unused hello function from WebSocket signal handler

--- packages/ws-signal/handler.ts
+++
@@ -1,8 +0,0 @@
-export const hello = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
-};
