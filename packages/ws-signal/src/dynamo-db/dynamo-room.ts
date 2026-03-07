dd3ef1d715023d8912061b91cc3efa86fd3dae90
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-07 22:12:27 +0900

    feat: DynamoDB用のルームスキーマとDAOを追加

--- packages/ws-signal/src/dynamo-db/dynamo-room.ts
+++ packages/ws-signal/src/dynamo-db/dynamo-room.ts
@@ -1,10 +1,15 @@
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { z } from "zod";
+
 import {
   RTCIceCandidateInit,
+  RTCIceCandidateInitSchema,
   RTCSessionDescriptionInit,
+  RTCSessionDescriptionInitSchema,
 } from "../core/web-rtc";
 
 /** DynamoDBスキーマ room */
-export type DynamoRoom = {
+export type DynamoRooms = {
   /** ルームID（パーティションキー） */
   roomID: string;
   /** ルームに入室可能であるか否か、trueで入室可能 */
@@ -17,3 +22,31 @@ export type DynamoRoom = {
     iceCandidates: RTCIceCandidateInit[];
   };
 };
+
+/** DynamoRooms zodスキーマ */
+export const DynamoRoomsSchema = z.object({
+  roomID: z.string(),
+  canEntry: z.boolean(),
+  hostSignal: z.object({
+    sdp: RTCSessionDescriptionInitSchema,
+    iceCandidates: z.array(RTCIceCandidateInitSchema),
+  }),
+});
+
+/** DynamoDB rooms の DAO */
+export class DynamoRoomsDAO {
+  /** DynamoDBDocument */
+  #dynamoDB: DynamoDBDocument;
+  /** DynamoDB テーブル名 */
+  #tableName: string;
+
+  /**
+   * コンストラクタ
+   * @param dynamoDB DynamoDBDocument
+   * @param tableName テーブル名
+   */
+  constructor(dynamoDB: DynamoDBDocument, tableName: string) {
+    this.#dynamoDB = dynamoDB;
+    this.#tableName = tableName;
+  }
+}
