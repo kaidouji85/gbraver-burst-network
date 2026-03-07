51970286295678844e45fd43e4b246bde06a438e
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-07 14:50:11 +0900

    feat: コネクションステートとスキーマを追加

--- packages/ws-signal/src/core/connection.ts
+++ packages/ws-signal/src/core/connection.ts
@@ -1,10 +1,20 @@
+import { z } from "zod";
+
 /** 状態なし */
 export type None = {
   type: "None";
 };
 
+/** None zodスキーマ */
+export const NoneSchema = z.object({
+  type: z.literal("None"),
+});
+
 /** コネクションステート */
 export type ConnectionState = None;
+
+/** ConnectionState zodスキーマ */
+export const ConnectionStateSchema = z.union([NoneSchema]);
 
 /** コネクション */
 export type Connection = {
@@ -13,3 +23,9 @@ export type Connection = {
   /** ステート */
   state: ConnectionState;
 };
+
+/** Connection zodスキーマ */
+export const ConnectionSchema = z.object({
+  connectionId: z.string(),
+  state: ConnectionStateSchema,
+});
