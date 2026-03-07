39d4b59bb13d776d347d13fea2b15090a4d1dcf8
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-07 15:10:06 +0900

    feat: DynamoDB用の新しい依存関係と接続クラスを追加

--- packages/ws-signal/src/dynamo-db/dynamo-connections.ts
+++ packages/ws-signal/src/dynamo-db/dynamo-connections.ts
@@ -1,10 +1,30 @@
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
 import { Connection, ConnectionSchema } from "../core/connection";
 
 /**
  * DynamoDB スキーマ connections
  * パーティションキー connectionId
  */
-export type DynamoConnections = Connection;
+export type DynamoConnection = Connection;
 
 /** DynamoConnections zodスキーマ */
-export const DynamoConnectionsSchema = ConnectionSchema;
+export const DynamoConnectionSchema = ConnectionSchema;
+
+/** DynamoDB connections の DAO */
+export class DynamoConnections {
+  /** DynamoDBDocument */
+  #dynamoDB: DynamoDBDocument;
+  /** DynamoDB テーブル名 */
+  #tableName: string;
+
+  /**
+   * コンストラクタ
+   * @param dynamoDB DynamoDBDocument
+   * @param tableName テーブル名
+   */
+  constructor(dynamoDB: DynamoDBDocument, tableName: string) {
+    this.#dynamoDB = dynamoDB;
+    this.#tableName = tableName;
+  }
+}
