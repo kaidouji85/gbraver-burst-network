d6ffc32100049481ae55c09ccbb4f89f9390c2c3
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-10 09:09:28 +0900

    feat: joinRoom関数を実装し、リクエストボディの検証とエラーハンドリングを追加

--- packages/ws-signal/src/join-room.ts
+++ packages/ws-signal/src/join-room.ts
@@ -3,6 +3,46 @@ import {
   APIGatewayProxyWebsocketEventV2,
 } from "aws-lambda";
 
+import { createAPIGatewayEndpoint } from "./api-gateway/endpoint";
+import { createApiGatewayManagementApi } from "./api-gateway/management";
+import { Notifier } from "./api-gateway/notifier";
+import { DynamoConnections } from "./dynamo-db/dynamo-connections";
+import { createDynamoDBDocument } from "./dynamo-db/dynamo-db-document";
+import { DynamoRooms } from "./dynamo-db/dynamo-rooms";
+import { JoinRoomSchema } from "./request/join-room";
+
+/** AWSリージョン */
+const AWS_REGION = process.env.AWS_REGION ?? "";
+/** ステージ */
+const STAGE = process.env.STAGE ?? "";
+/** Websocket API ID */
+const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
+/** DynamoDB connection テーブル名 */
+const DYNAMODB_CONNECTIONS_TABLE = process.env.DYNAMODB_CONNECTIONS_TABLE ?? "";
+/** DynamoDB rooms テーブル名 */
+const DYNAMODB_ROOMS_TABLE = process.env.DYNAMODB_ROOMS_TABLE ?? "";
+
+/** API エンドポイント */
+const apiGatewayEndpoint = createAPIGatewayEndpoint(
+  WEBSOCKET_API_ID,
+  AWS_REGION,
+  STAGE,
+);
+/** API Gateway Management API */
+const apiGateway = createApiGatewayManagementApi(apiGatewayEndpoint);
+/** WebSocket用メッセージ通知オブジェクト */
+const notifier = new Notifier(apiGateway);
+
+/** DynamoDB ドキュメントクライアント */
+const dynamoDB = createDynamoDBDocument(AWS_REGION);
+/** DynamoDB connections DAO */
+const dynamoConnections = new DynamoConnections(
+  dynamoDB,
+  DYNAMODB_CONNECTIONS_TABLE,
+);
+/** DynamoDB rooms DAO */
+const dynamoRooms = new DynamoRooms(dynamoDB, DYNAMODB_ROOMS_TABLE);
+
 /**
  * ゲストが入室する
  * @param event イベント
@@ -11,5 +51,13 @@ import {
 export async function joinRoom(
   event: APIGatewayProxyWebsocketEventV2,
 ): Promise<APIGatewayProxyResultV2> {
+  const parsedBody = JSON.parse(event.body || "");
+  const joinRoom = JoinRoomSchema.safeParse(parsedBody);
+  const { connectionId } = event.requestContext;
+  if (!joinRoom.success) {
+    await notifier.notifyToClient(connectionId, { type: "join-room-rejected" });
+    return { statusCode: 400, body: "invalid request body" };
+  }
+
   return { statusCode: 200, body: "join room success" };
 }
