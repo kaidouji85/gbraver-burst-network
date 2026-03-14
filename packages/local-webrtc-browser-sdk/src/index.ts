37a0ac60796252524e65773e67c605ce598fb9da
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-14 20:23:38 +0900

    fix: local-webrtc-hostのエクスポートのフォーマットを修正

--- packages/local-webrtc-browser-sdk/src/index.ts
+++ packages/local-webrtc-browser-sdk/src/index.ts
@@ -2,4 +2,4 @@ export {
   createLocalWebRTCBrowserSDK,
   LocalWebRTCBrowserSDK,
 } from "./local-webrtc-guest";
-export { createLocalWebRTCHost,LocalWebRTCHost } from "./local-webrtc-host";
+export { createLocalWebRTCHost, LocalWebRTCHost } from "./local-webrtc-host";
