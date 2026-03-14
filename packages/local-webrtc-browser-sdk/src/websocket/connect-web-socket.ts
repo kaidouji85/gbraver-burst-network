9c68790b803838c151b67a1b25b6234b2c950fc1
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-14 19:17:14 +0900

    feat: WebSocket接続を管理するconnectWebSocket関数を追加

---
+++ packages/local-webrtc-browser-sdk/src/websocket/connect-web-socket.ts
@@ -0,0 +1,26 @@
+/**
+ * 接続完了したWebSocketを返す
+ * @param url 接続先のWebSocket
+ * @returns WebSocket
+ */
+export function connectWebSocket(url: string): Promise<WebSocket> {
+  let handler: (() => void) | null = null;
+  let errorHandler: ((event: Event) => void) | null = null;
+  const websocket = new WebSocket(url);
+  return new Promise<WebSocket>((resolve, reject) => {
+    handler = () => {
+      resolve(websocket);
+    };
+
+    errorHandler = reject;
+    websocket.addEventListener("open", handler);
+    websocket.addEventListener("error", errorHandler);
+  }).finally(() => {
+    if (handler) {
+      websocket.removeEventListener("open", handler);
+    }
+    if (errorHandler) {
+      websocket.removeEventListener("error", errorHandler);
+    }
+  });
+}
