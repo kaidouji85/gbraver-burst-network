c6916bb55959b24c553d4cda329f3c030ed1f1f8
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-14 10:41:34 +0900

    refactor: parseJSON関数の戻り値をunknownに変更し、コメントを更新

--- packages/backend-app/src/json/parse.ts
+++ packages/backend-app/src/json/parse.ts
@@ -1,10 +1,10 @@
 /**
- * JSONパースを行う
- * JSONパースできなかった場合はnullを返す
+ * 例外を発生させずにJSONをパースする
+ * パース結果はzodなどでバリデーションすることを想定している
  * @param origin パース元
  * @returns パース結果
  */
-export function parseJSON(origin: unknown): unknown | null {
+export function parseJSON(origin: unknown): unknown {
   if (typeof origin !== "string") {
     return null;
   }
