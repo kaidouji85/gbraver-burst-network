6cd776d37f1c89213b9161f609a2cb4c5a02dc29
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-15 12:04:33 +0900

    feat: ユースケースセレクター、実行ボタン、ルームID入力欄を取得する関数を追加

--- packages/local-webrtc-stub/src/index.ts
+++ packages/local-webrtc-stub/src/index.ts
@@ -3,21 +3,73 @@ import { HostPlayer } from "./use-case/host-player";
 import { UseCase } from "./use-case/use-case";
 
 /**
+ * ユースケースセレクターを取得する
+ * @returns 取得結果、セレクターが見つからない場合は新規作成したセレクター
+ */
+const getUseCaseSelector = (): HTMLSelectElement => {
+  const foundUseCaseSelector = document.getElementById("use-case-selector");
+  return foundUseCaseSelector instanceof HTMLSelectElement
+    ? foundUseCaseSelector
+    : document.createElement("select");
+};
+
+/**
+ * ユースケース実行ボタンを取得する
+ * @returns 取得結果、ボタンが見つからない場合は新規作成したボタン
+ */
+const getUseCaseExecuteButton = (): HTMLButtonElement => {
+  const foundUseCaseExecuteButton = document.getElementById(
+    "use-case-execute-button",
+  );
+  return foundUseCaseExecuteButton instanceof HTMLButtonElement
+    ? foundUseCaseExecuteButton
+    : document.createElement("button");
+};
+
+/**
+ * ルームID入力欄を取得する
+ * @returns 取得結果、入力欄が見つからない場合は新規作成した入力欄
+ */
+const getRoomIDInput = (): HTMLInputElement => {
+  const foundRoomIDInput = document.getElementById("room-id");
+  return foundRoomIDInput instanceof HTMLInputElement
+    ? foundRoomIDInput
+    : document.createElement("input");
+};
+
+/**
  * エントリポイント
  */
 window.onload = () => {
   const useCases: UseCase[] = [new HostPlayer(), new GuestPlayer()];
 
-  const foundUseCaseSelector = document.getElementById("use-case-selector");
-  const useCaseSelector: HTMLSelectElement =
-    foundUseCaseSelector instanceof HTMLSelectElement
-      ? foundUseCaseSelector
-      : document.createElement("select");
+  const useCaseSelector = getUseCaseSelector();
+  const useCaseExecuteButton = getUseCaseExecuteButton();
+  const roomIDInput = getRoomIDInput();
 
   useCases.forEach((v, index) => {
     const item = document.createElement("option");
     item.innerText = v.name();
     item.value = index.toString();
     useCaseSelector.appendChild(item);
+  });
+
+  useCaseExecuteButton.addEventListener("click", async (e: Event) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const useCaseIndex = Number(useCaseSelector.value);
+    if (isNaN(useCaseIndex)) {
+      return;
+    }
+
+    const useCase = useCases[useCaseIndex];
+    if (!useCase) {
+      return;
+    }
+
+    useCaseExecuteButton.disabled = true;
+    const roomID = roomIDInput.value;
+    await useCase.execute({ roomID });
+    useCaseExecuteButton.disabled = false;
   });
 };
