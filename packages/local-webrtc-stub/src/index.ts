190301885622d6a99632655edc7f53515d1076b5
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-15 11:57:43 +0900

    feat: local-webrtc-stubのエントリポイントを更新し、UseCaseの選択肢を追加

--- packages/local-webrtc-stub/src/index.ts
+++ packages/local-webrtc-stub/src/index.ts
@@ -1,6 +1,22 @@
+import { HostPlayer } from "./use-case/host-player";
+import { UseCase } from "./use-case/use-case";
+
 /**
  * エントリポイント
  */
 window.onload = () => {
-  console.log("local-webrtc-stub loaded");
+  const useCases: UseCase[] = [new HostPlayer()];
+
+  const foundUseCaseSelector = document.getElementById("use-case-selector");
+  const useCaseSelector: HTMLSelectElement =
+    foundUseCaseSelector instanceof HTMLSelectElement
+      ? foundUseCaseSelector
+      : document.createElement("select");
+
+  useCases.forEach((v, index) => {
+    const item = document.createElement("option");
+    item.innerText = v.name();
+    item.value = index.toString();
+    useCaseSelector.appendChild(item);
+  });
 };
