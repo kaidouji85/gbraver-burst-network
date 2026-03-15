a1a78c7854696425987003d6c034fdf86a5e9f0c
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-15 14:20:13 +0900

    feat: index.tsからLocalWebRTCRoomの重複エクスポートを削除

--- packages/local-webrtc-browser-sdk/src/index.ts
+++ packages/local-webrtc-browser-sdk/src/index.ts
@@ -1,8 +1,8 @@
+export { LocalWebRTCRoom } from "./sdk/local-web-rtc-room";
 export {
   createLocalWebRTCGuestSDK,
   LocalWebRTCGuestSDK,
 } from "./sdk/local-webrtc-guest-sdk";
-export { LocalWebRTCRoom } from "./sdk/local-web-rtc-room";
 export {
   createLocalWebRTCHostSDK,
   LocalWebRTCHostSDK,
