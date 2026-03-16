067d2b761050b83f4fafdc323cb57fb293ce4c76
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-17 08:43:14 +0900

    feat: JoinRoom型からsdpおよびiceCandidatesフィールドを削除し、SendGuestSignalをWSSignalRequestに追加

--- packages/local-webrtc-browser-sdk/src/ws-signal/join-room.ts
+++ packages/local-webrtc-browser-sdk/src/ws-signal/join-room.ts
@@ -1,33 +1,30 @@
 import { parseJSON } from "../json/parse";
 import { Signal } from "../webrtc/signal";
+import { JoinRoomAcceptedSchema } from "./response/join-room-accepted";
 import { JoinRoomRejectedSchema } from "./response/join-room-rejected";
-import { MatchingSchema } from "./response/matching";
 import { sendToWSSignal } from "./send-to-ws-signal";
 
 /**
  * ルームに参加する
  * @param websocket WebSocketコネクション
  * @param roomID 参加するルームのID
- * @param sdp 自身のSDP
- * @param iceCandidates 自身のICE候補
  * @return ルームへの参加に成功したらSignal、失敗したらnull
  */
 export const joinRoom = (options: {
   websocket: WebSocket;
   roomID: string;
-  sdp: RTCSessionDescriptionInit;
-  iceCandidates: RTCIceCandidateInit[];
 }): Promise<Signal | null> => {
-  const { websocket, roomID, sdp, iceCandidates } = options;
+  const { websocket, roomID } = options;
   let handler: ((event: MessageEvent) => void) | null = null;
   return new Promise<Signal | null>((resolve) => {
     handler = (event) => {
       const parsedData = parseJSON(event.data);
 
-      const parsedMatching = MatchingSchema.safeParse(parsedData);
-      if (parsedMatching.success) {
-        const matching = parsedMatching.data;
-        const { sdp, iceCandidates } = matching;
+      const parsedJoinRoomAccepted =
+        JoinRoomAcceptedSchema.safeParse(parsedData);
+      if (parsedJoinRoomAccepted.success) {
+        const joinRoomAccepted = parsedJoinRoomAccepted.data;
+        const { sdp, iceCandidates } = joinRoomAccepted;
         resolve({ sdp, iceCandidates });
         return;
       }
@@ -40,12 +37,7 @@ export const joinRoom = (options: {
       }
     };
     websocket.addEventListener("message", handler);
-    sendToWSSignal(websocket, {
-      action: "join-room",
-      roomID,
-      sdp,
-      iceCandidates,
-    });
+    sendToWSSignal(websocket, { action: "join-room", roomID });
   }).finally(() => {
     if (handler) {
       websocket.removeEventListener("message", handler);
