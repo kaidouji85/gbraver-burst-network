462c9a3ab95f4240daef055707477a5c6348ee80
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-20 10:22:36 +0900

    feat: WebSocketConnectionManagerクラスを追加し、WebSocketの接続管理機能を実装

---
+++ packages/local-webrtc-browser-sdk/src/sdk/websocket-connection-manager.ts
@@ -0,0 +1,65 @@
+import { fromEvent, Observable, Subject, Subscription } from "rxjs";
+import { connectWSSignal } from "../ws-signal/connect-ws-signal";
+
+export class WebSocketConnectionManager {
+  /** WebSocketシグナルサーバーのURL */
+  readonly wsSignalUrl: string;
+  /** WebSocketコネクション */
+  #websocket: WebSocket | null = null;
+  /** Web Socket エラー通知 */
+  #websocketError: Subject<unknown>;
+  /** Web Socket イベントストリーム */
+  #websocketSubscriptions: Subscription[];
+
+  /**
+   * コンストラクタ
+   * @param wsSignalUrl WebSocketシグナルサーバーのURL
+   */
+  constructor(wsSignalUrl: string) {
+    this.wsSignalUrl = wsSignalUrl;
+    this.#websocketError = new Subject();
+    this.#websocketSubscriptions = [];
+  }
+
+  /**
+   * WebSocketクライアントの取得を行う
+   * WebSocketクライアントが存在しない場合は、本メソッド内で生成してから返す
+   * @returns 取得、生成結果
+   */
+  async getOrCreateWebSocket(): Promise<WebSocket> {
+    if (this.#websocket) {
+      return this.#websocket;
+    }
+
+    const websocket = await connectWSSignal(this.wsSignalUrl);
+    this.#websocketSubscriptions = [
+      fromEvent(websocket, "error").subscribe(this.#websocketError),
+      fromEvent(websocket, "close").subscribe(this.#websocketError),
+    ];
+    this.#websocket = websocket;
+    return websocket;
+  }
+
+  /**
+   * Websocketコネクションを切断する
+   * 本メソッドを呼び出すことで、WebSocketコネクションに関連するイベントストリームの購読も解除する
+   * @returns 処理が完了したら発火するPromise
+   */
+  async gracefulDisconnectWebsocket(): Promise<void> {
+    this.#websocket?.close();
+    this.#websocket = null;
+
+    this.#websocketSubscriptions.forEach((v) => {
+      v.unsubscribe();
+    });
+    this.#websocketSubscriptions = [];
+  }
+
+  /**
+   * WebSocketのエラーを通知する
+   * @returns 通知ストリーム
+   */
+  websocketErrorNotifier(): Observable<unknown> {
+    return this.#websocketError;
+  }
+}
