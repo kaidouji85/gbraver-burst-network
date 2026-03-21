c00d3becdf78bbfe519e36dc9e4b73f0543513bf
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-21 13:23:02 +0900

    feat: ホスト側のWebRTCコネクション管理クラスを追加

---
+++ packages/local-webrtc-browser-sdk/src/webrtc/host-webrtc-connection-manager.ts
@@ -0,0 +1,57 @@
+/** 接続中 */
+type Connected = {
+  type: "connected";
+  /** WebRTCコネクション */
+  connection: RTCPeerConnection;
+  /** データチャンネル */
+  dataChannel: RTCDataChannel;
+};
+
+/** 切断中 */
+type Disconnected = {
+  type: "disconnected";
+};
+
+/** コネクションの状態 */
+type ConnectionState = Connected | Disconnected;
+
+/** ホスト側のコネクション */
+export class HostWebRTCConnectionManager {
+  /** コネクションの状態 */
+  #connectionState: ConnectionState = { type: "disconnected" };
+
+  /**
+   * コネクションを取得する。
+   * コネクションが存在しない場合は新たに作成する。
+   * @returns 取得したコネクションとデータチャンネル
+   */
+  getOrCreateConnection(): {
+    /** コネクション */
+    connection: RTCPeerConnection;
+    /** データチャンネル */
+    dataChannel: RTCDataChannel;
+  } {
+    if (this.#connectionState.type === "disconnected") {
+      const connection = new RTCPeerConnection();
+      const dataChannel = connection.createDataChannel("sendDataChannel");
+      this.#connectionState = {
+        type: "connected",
+        connection,
+        dataChannel,
+      };
+    }
+
+    return this.#connectionState;
+  }
+
+  /**
+   * コネクションを切断する
+   */
+  disconnect() {
+    if (this.#connectionState.type === "connected") {
+      this.#connectionState.connection.close();
+      this.#connectionState.dataChannel.close();
+    }
+    this.#connectionState = { type: "disconnected" };
+  }
+}
