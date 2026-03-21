38e6e304d6cf3bd2c32f105f73c9c52f99b34622
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-21 13:07:14 +0900

    feat: データチャネルが開通するのを待つ関数を追加

---
+++ packages/local-webrtc-browser-sdk/src/webrtc/wait-until-data-channel.ts
@@ -0,0 +1,17 @@
+/**
+ * ホスト側でデータチャネルが開通するのを待つ
+ * @param connection ホスト側のWebRTCコネクション
+ * @returns 開通したデータチャネル
+ */
+export const waitUntilDataChannel = (
+  connection: RTCPeerConnection,
+): Promise<RTCDataChannel> =>
+  new Promise((resolve) => {
+    connection.addEventListener(
+      "datachannel",
+      (event) => {
+        resolve(event.channel);
+      },
+      { once: true },
+    );
+  });
