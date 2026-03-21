a1d7f4fa6f874fb179674b609d49db8ef7ac0c73
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-21 19:17:43 +0900

    feat: ゲストおよびホスト側でバトル進行処理を追加し、進行メソッドを更新

--- packages/local-webrtc-stub/src/use-case/guest-player.ts
+++ packages/local-webrtc-stub/src/use-case/guest-player.ts
@@ -25,11 +25,44 @@ export class GuestPlayer implements UseCase {
   async execute(context: UseCaseContext): Promise<void> {
     const { roomID } = context;
     console.log("start join room, roomID:", roomID);
-    const isJoined = await this.#guestSDK.joinRoom({
+    const battle = await this.#guestSDK.joinRoom({
       roomID,
       armdozerId: ArmdozerIds.NEO_LANDOZER,
       pilotId: PilotIds.GAI,
     });
-    console.log("join room result:", isJoined);
+    if (!battle) {
+      console.log("Failed to join room");
+      return;
+    }
+
+    battle.suddenlyBattleEndNotifier().subscribe(() => {
+      console.log("suddenly battle end");
+    });
+    console.log("start battle", battle);
+    const update01 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 3,
+    });
+    console.log(update01);
+    const update02 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update02);
+    const update03 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update03);
+    const update04 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 0,
+    });
+    console.log(update04);
+    const update05 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 0,
+    });
+    console.log(update05);
   }
 }
--- packages/local-webrtc-stub/src/use-case/host-player.ts
+++ packages/local-webrtc-stub/src/use-case/host-player.ts
@@ -34,7 +34,30 @@ export class HostPlayer implements UseCase {
     }
 
     console.log("create room success, roomID:", room.roomID);
-    const guestSignal = await room.waitUntilMatching();
-    console.log("matching success, guestSignal:", guestSignal);
+    const battle = await room.waitUntilMatching();
+    console.log("start battle", battle);
+    const update01 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update01);
+    const update02 = await battle.progress({
+      type: "BURST_COMMAND",
+    });
+    console.log(update02);
+    const update03 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update03);
+    const update04 = await battle.progress({
+      type: "PILOT_SKILL_COMMAND",
+    });
+    console.log(update04);
+    const update05 = await battle.progress({
+      type: "BATTERY_COMMAND",
+      battery: 5,
+    });
+    console.log(update05);
   }
 }
