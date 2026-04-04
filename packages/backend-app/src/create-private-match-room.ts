56aa6ade5af6af1ffc5e7ff62c805c12086885fe
Y.Takeuchi <kaidouji85@gmail.com>
2026-04-04 13:27:50 +0900

    feat: ルーム生成にリトライ機能を追加し、ユーザー情報を利用してルームを作成

--- packages/backend-app/src/create-private-match-room.ts
+++ packages/backend-app/src/create-private-match-room.ts
@@ -3,6 +3,7 @@ import { createApiGatewayManagementApi } from "./api-gateway/management";
 import { Notifier } from "./api-gateway/notifier";
 import { generatePrivateMatchRoomID } from "./core/generate-private-match-room-id";
 import { PrivateMatchRoom } from "./core/private-match-room";
+import { User } from "./core/user";
 import { createDynamoConnections } from "./dynamo-db/create-dynamo-connections";
 import { createDynamoPrivateMatchRooms } from "./dynamo-db/create-dynamo-private-match-rooms";
 import { createDynamoDBDocument } from "./dynamo-db/dynamo-db-document";
@@ -10,7 +11,10 @@ import { parseJSON } from "./json/parse";
 import { extractUserFromWebSocketAuthorizer } from "./lambda/extract-user";
 import { WebsocketAPIEvent } from "./lambda/websocket-api-event";
 import { WebsocketAPIResponse } from "./lambda/websocket-api-response";
-import { parseCreatePrivateMatchRoom } from "./request/create-private-match-room";
+import {
+  CreatePrivateMatchRoom,
+  parseCreatePrivateMatchRoom,
+} from "./request/create-private-match-room";
 import { Error } from "./response/error";
 
 /** AWSリージョン */
@@ -21,6 +25,8 @@ const SERVICE = process.env.SERVICE ?? "";
 const STAGE = process.env.STAGE ?? "";
 /** WebSocket API ID */
 const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
+/** ルーム作成リトライ回数 */
+const MAX_ROOM_CREATION_RETRY = 5;
 
 /** DynamoDB ドキュメントクライアント */
 const dynamoDB = createDynamoDBDocument(AWS_REGION);
@@ -49,6 +55,38 @@ const invalidRequestBodyError: Error = {
   action: "error",
   error: "invalid request body",
 };
+
+/**
+ * リトライありでルーム生成をする
+ * @param options ルーム生成オプション
+ * @param options.connectionId コネクションID
+ * @param options.data リクエストボディ
+ * @param options.user ユーザー情報
+ * @returns 生成できた場合はルーム情報、生成できなかった場合はnull
+ */
+async function createRoomWithRetry(options: {
+  connectionId: string;
+  data: CreatePrivateMatchRoom;
+  user: User;
+}): Promise<PrivateMatchRoom | null> {
+  const { connectionId: ownerConnectionId, data, user } = options;
+  const { armdozerId, pilotId } = data;
+  const { userID: owner } = user;
+  for (let i = 0; i < MAX_ROOM_CREATION_RETRY; i++) {
+    const room: PrivateMatchRoom = {
+      roomID: generatePrivateMatchRoomID(),
+      owner,
+      ownerConnectionId,
+      armdozerId,
+      pilotId,
+    };
+    const isRoomCreationSuccessful = await dynamoPrivateMatchRooms.put(room);
+    if (isRoomCreationSuccessful) {
+      return room;
+    }
+  }
+  return null;
+}
 
 /**
  * Websocket API createPrivateMatchRoom エントリポイント
