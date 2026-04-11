68882f9f9ce2819aebb9722b4e021af18af59fd9
Y.Takeuchi <kaidouji85@gmail.com>
2026-04-11 23:01:34 +0900

    feat: coturn用のクレデンシャル発行機能を実装し、レスポンス形式を更新

--- packages/ws-signal/src/issue-credential.ts
+++ packages/ws-signal/src/issue-credential.ts
@@ -1,20 +1,32 @@
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
-import { APIGatewayProxyEventV2, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyResult } from "aws-lambda";
+import { createHmac } from "crypto";
 
 /** coturnサーバーとの共有秘密鍵が格納されているAWS Secrets Managerのシークレット名 */
 const COTURN_SHARED_SECRET = process.env.COTURN_SHARED_SECRET ?? "";
+/** coturnクレデンシャル有効期限（秒） */
+const COTURN_CREDENTIAL_TTL_SECONDS = 60 * 15;
 
 /** coturnサーバーとの共有秘密鍵のPromise */
 const coturnSharedSecretPromise = getSecret(COTURN_SHARED_SECRET);
 
 /**
  * coturn用のクレデンシャルを発行する
- * @param event イベント
  * @returns レスポンス
  */
-export const issueCoturnCredential = async (
-  event: APIGatewayProxyEventV2,
-): Promise<APIGatewayProxyResult> => {
-  const coturnSharedSecret = await coturnSharedSecretPromise;
-  return { statusCode: 201, body: "hello" };
-};
+export const issueCoturnCredential =
+  async (): Promise<APIGatewayProxyResult> => {
+    const coturnSharedSecret = String(await coturnSharedSecretPromise);
+    const expiresAt =
+      Math.floor(Date.now() / 1000) + COTURN_CREDENTIAL_TTL_SECONDS;
+    const username = `${expiresAt}:webrtc-user`;
+    const password = createHmac("sha1", coturnSharedSecret)
+      .update(username)
+      .digest("base64");
+
+    return {
+      statusCode: 201,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    };
+  };
