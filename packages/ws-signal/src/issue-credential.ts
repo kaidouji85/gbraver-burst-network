3116d34fbe4ee0f94b3ffd65530e99cf24c3648c
Y.Takeuchi <kaidouji85@gmail.com>
2026-04-11 20:11:01 +0900

    feat: COTURNの共有秘密鍵をAWS Secrets Managerから取得する機能を追加

--- packages/ws-signal/src/issue-credential.ts
+++ packages/ws-signal/src/issue-credential.ts
@@ -1,4 +1,11 @@
+import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
 import { APIGatewayProxyEventV2, APIGatewayProxyResult } from "aws-lambda";
+
+/** coturnサーバーとの共有秘密鍵が格納されているAWS Secrets Managerのシークレット名 */
+const COTURN_SHARED_SECRET = process.env.COTURN_SHARED_SECRET ?? "";
+
+/** coturnサーバーとの共有秘密鍵のPromise */
+const coturnSharedSecretPromise = getSecret(COTURN_SHARED_SECRET);
 
 /**
  * coturn用のクレデンシャルを発行する
@@ -8,8 +15,6 @@ import { APIGatewayProxyEventV2, APIGatewayProxyResult } from "aws-lambda";
 export const issueCoturnCredential = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 201,
-    body: "hello",
-  };
+  const coturnSharedSecret = await coturnSharedSecretPromise;
+  return { statusCode: 201, body: "hello" };
 };
