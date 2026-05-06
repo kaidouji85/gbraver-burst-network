1a9777d5813874201183bbfc4216ee405165bffb
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-06 15:30:25 +0900

    DynamoDB authTokensテーブルを環境変数に追加し、issueAuthToken関数で認証トークンをDynamoDBに保存する機能を実装

--- packages/ws-signal/src/issue-auth-token.ts
+++ packages/ws-signal/src/issue-auth-token.ts
@@ -1,6 +1,23 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda";
 
 import { createAuthToken } from "./core/auth-token";
+import { createDynamoDBDocument } from "./dynamo-db/dynamo-db-document";
+import { DynamoAuthTokens } from "./dynamo-db/dynamo-auth-tokens";
+
+/** AWSリージョン */
+const AWS_REGION = process.env.AWS_REGION ?? "";
+/** ステージ */
+const STAGE = process.env.STAGE ?? "";
+/** DynamoDB authTokens テーブル名 */
+const DYNAMO_AUTH_TOKENS_TABLE = process.env.DYNAMODB_AUTH_TOKENS_TABLE ?? "";
+
+/** DynamoDB ドキュメントクライアント */
+const dynamoDB = createDynamoDBDocument(AWS_REGION);
+/** DynamoDB authTokens DAO */
+const dynamoAuthTokens = new DynamoAuthTokens(
+  dynamoDB,
+  DYNAMO_AUTH_TOKENS_TABLE,
+);
 
 /**
  * 認証トークンを発行する
@@ -8,6 +25,7 @@ import { createAuthToken } from "./core/auth-token";
  */
 export const issueAuthToken = async (): Promise<APIGatewayProxyResultV2> => {
   const authToken = createAuthToken();
+  await dynamoAuthTokens.put(authToken);
   return {
     statusCode: 201,
     headers: {
