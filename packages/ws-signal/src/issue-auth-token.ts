58e4dbdd7eb703bd29494bcde90baf812b00fcb8
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-06 15:15:36 +0900

    issueAuthToken関数で認証トークンをダミーから実際のトークン生成に変更

--- packages/ws-signal/src/issue-auth-token.ts
+++ packages/ws-signal/src/issue-auth-token.ts
@@ -1,15 +1,18 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda";
 
+import { createAuthToken } from "./core/auth-token";
+
 /**
  * 認証トークンを発行する
  * @returns HTTPレスポンス
  */
 export const issueAuthToken = async (): Promise<APIGatewayProxyResultV2> => {
+  const authToken = createAuthToken();
   return {
     statusCode: 201,
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify({ token: "dummy-token" }),
+    body: JSON.stringify(authToken),
   };
 };
