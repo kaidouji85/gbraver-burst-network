2c73ddc2b74417ecbc8c5e7e5afa20e6f5df55a1
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-09 17:03:20 +0900

    オーソライザー関数にイベント引数を追加し、型定義を適用

--- packages/ws-signal/src/authorizer.ts
+++ packages/ws-signal/src/authorizer.ts
@@ -1,7 +1,15 @@
+import type {
+  APIGatewayProxyEventV2,
+  APIGatewaySimpleAuthorizerResult,
+} from "aws-lambda";
+
 /**
  * HTTP API用のオーソライザー
  * @returns 認証結果
  */
-export const authorizer = async () => {
+export const authorizer = async (
+  event: APIGatewayProxyEventV2,
+): Promise<APIGatewaySimpleAuthorizerResult> => {
+  //event.headers.authorization;
   return { isAuthorized: true };
 };
