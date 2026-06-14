d64d8b9218d85f5ffd09dea915b91db7878ec635
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-14 11:17:23 +0900

    deploy-backend-cloudfont.bashのスタック名とリージョンの引数を引用符で囲み、パラメータの値も引用符で囲むように修正

--- deploy-backend-cloudfont.bash
+++ deploy-backend-cloudfont.bash
@@ -9,11 +9,11 @@ EXPORT_NAME_OF_WEB_RTC_HELPER_DOMAIN_NAME="${WS_SIGNAL_SERVICE:?}:${STAGE:?}:Htt
 WEB_RTC_HELPER_DOMAIN_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='${EXPORT_NAME_OF_WEB_RTC_HELPER_DOMAIN_NAME}'].Value" --output text)
 aws cloudformation deploy \
   --template-file gb-backend-cloudfront.yaml \
-  --stack-name ${STACK_NAME} \
-  --region ${AWS_DEFAULT_REGION:?} \
+  --stack-name "${STACK_NAME}" \
+  --region "${AWS_DEFAULT_REGION:?}" \
   --parameter-overrides \
-  WebRTCHelperDomainName=${WEB_RTC_HELPER_DOMAIN_NAME} \
-  DistributionAlias=${BACKEND_CLOUDFRONT_DOMAIN_NAME:?} \
-  AcmCertificateArn=${BACKEND_CLOUDFRONT_CERT_ARN:?} \
-  WebACLArn=${BACKEND_CLOUDFRONT_WEB_ACL_ARN:?} \
+  WebRTCHelperDomainName="${WEB_RTC_HELPER_DOMAIN_NAME}" \
+  DistributionAlias="${BACKEND_CLOUDFRONT_DOMAIN_NAME:?}" \
+  AcmCertificateArn="${BACKEND_CLOUDFRONT_CERT_ARN:?}" \
+  WebACLArn="${BACKEND_CLOUDFRONT_WEB_ACL_ARN:?}" \
   --no-fail-on-empty-changeset
