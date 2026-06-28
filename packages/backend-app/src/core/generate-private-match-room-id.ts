a18c6016c7a1a2c1409521d9526decbd982f3756
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-28 19:39:18 +0900

    プライベートマッチのルームID生成機能を改善し、NGワードを考慮したランダム生成を実装

--- packages/backend-app/src/core/generate-private-match-room-id.ts
+++ packages/backend-app/src/core/generate-private-match-room-id.ts
@@ -22,12 +22,40 @@ const kana = [
 /** ルームIDの文字数 */
 const ROOM_ID_LENGTH = 5;
 
+/** ルームIDの最大生成試行回数 */
+const MAX_ROOM_CREATION_RETRY = 5;
+
+/**
+ * デフォルトのNGワード
+ * NGワードをコードに直接記載するのは憚られたので、Unicodeエスケープシーケンスで記載している
+ */
+const DEFAULT_NG_WORDS = [
+  "\u3057\u306d",
+  "\u3053\u308d\u3059",
+  "\u3061\u3093",
+  "\u307e\u3093",
+];
+
 /**
  * プライベートマッチのルームIDを生成する
+ * @param ngWords NGワードの配列。指定しない場合はデフォルトのNGワードが使用される
  * @returns 生成されたルームID
  */
-export const generatePrivateMatchRoomID = (): PrivateMatchRoomID =>
-  Array.from(
-    { length: ROOM_ID_LENGTH },
-    () => kana[crypto.randomInt(kana.length)],
-  ).join("");
+export const generatePrivateMatchRoomID = (
+  ngWords: string[] = DEFAULT_NG_WORDS,
+): PrivateMatchRoomID => {
+  for (let i = 0; i < MAX_ROOM_CREATION_RETRY; i++) {
+    const roomID = Array.from(
+      { length: ROOM_ID_LENGTH },
+      () => kana[crypto.randomInt(kana.length)],
+    ).join("");
+    const hasNGWord = ngWords.some((ngWord) => roomID.includes(ngWord));
+    if (!hasNGWord) {
+      return roomID;
+    }
+  }
+
+  throw new Error(
+    "failed to create private match room ID after maximum retries",
+  );
+};
