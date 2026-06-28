7e571ef2f0e107d52e46673b66cc9598d09d7960
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-28 19:15:28 +0900

    ルームID生成機能を改善し、NGワードを考慮したランダム生成を実装

--- packages/ws-signal/src/core/create-room-id.ts
+++ packages/ws-signal/src/core/create-room-id.ts
@@ -20,12 +20,36 @@ const kana = [
 /** ルームIDの文字数 */
 const ROOM_ID_LENGTH = 5;
 
+/** ルームIDの最大生成試行回数 */
+const MAX_ROOM_CREATION_RETRY = 5;
+
+/**
+ * デフォルトのNGワード
+ * NGワードをコードに直接記載するのは憚られたので、Unicodeエスケープシーケンスで記載している
+ */
+const DEFAULT_NG_WORDS = [
+  "\u3057\u306d",
+  "\u3053\u308d\u3059",
+  "\u3061\u3093",
+  "\u307e\u3093",
+];
+
 /**
  * ランダムなルームIDを生成する
+ * @param ngWords NGワードの配列。指定しない場合はデフォルトのNGワードが使用される
  * @return 生成されたルームID
  */
-export const createRoomID = (): string =>
-  Array.from(
-    { length: ROOM_ID_LENGTH },
-    () => kana[crypto.randomInt(kana.length)],
-  ).join("");
+export const createRoomID = (ngWords: string[] = DEFAULT_NG_WORDS): string => {
+  for (let i = 0; i < MAX_ROOM_CREATION_RETRY; i++) {
+    const roomID = Array.from(
+      { length: ROOM_ID_LENGTH },
+      () => kana[crypto.randomInt(kana.length)],
+    ).join("");
+    const hasNGWord = ngWords.some((ngWord) => roomID.includes(ngWord));
+    if (!hasNGWord) {
+      return roomID;
+    }
+  }
+
+  throw new Error("failed to create room ID after maximum retries");
+};
