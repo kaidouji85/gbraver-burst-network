0209466f15eab9e4cffc8269d31f326f72740ece
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-07 14:28:20 +0900

    fix: remove async keyword from getOrIssueAuthToken method

--- packages/local-webrtc-browser-sdk/src/sdk/auth-token-manager.ts
+++ packages/local-webrtc-browser-sdk/src/sdk/auth-token-manager.ts
@@ -52,7 +52,7 @@ class AuthTokenManagerImpl implements AuthTokenManager {
   }
 
   /** @override */
-  async getOrIssueAuthToken(): Promise<AuthToken> {
+  getOrIssueAuthToken(): Promise<AuthToken> {
     if (this.#authTokenPromise === null) {
       this.#authTokenPromise = this.#issueAuthTokenWithReset();
       return this.#authTokenPromise;
