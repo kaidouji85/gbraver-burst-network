770b46ae5ba66ad7d744a7e561ebd755c176a80d
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-07 15:17:50 +0900

    feat: add ICECandidateError type for improved error logging

--- packages/local-webrtc-browser-sdk/src/http-api/request/frontend-log.ts
+++ packages/local-webrtc-browser-sdk/src/http-api/request/frontend-log.ts
@@ -12,6 +12,15 @@ export type SignalingEnd = {
   spanId: string;
 };
 
+/** ログ：ICE候補収集エラー */
+export type ICECandidateError = {
+  type: "ICE_CANDIDATE_ERROR";
+  /** スパンID */
+  spanId: string;
+  /** エラーメッセージ */
+  error: string;
+};
+
 /** ログ：ICE候補送信開始 */
 export type ICECandidateStart = {
   type: "ICE_CANDIDATE_START";
@@ -28,4 +37,8 @@ export type ICECandidateEnd = {
 
 /** フロントエンドログ */
 export type FrontendLog =
-  ICECandidateStart | ICECandidateEnd | SignalingStart | SignalingEnd;
+  | ICECandidateStart
+  | ICECandidateEnd
+  | ICECandidateError
+  | SignalingStart
+  | SignalingEnd;
