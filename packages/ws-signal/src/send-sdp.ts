fdb35b91522d64a05a60edc66fbc33c631b1c6e4
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-13 23:35:07 +0900

    feat: implement sendSDP function with validation and rejection handling

--- packages/ws-signal/src/send-sdp.ts
+++ packages/ws-signal/src/send-sdp.ts
@@ -1,10 +1,50 @@
-import { APIGatewayProxyResultV2 } from "aws-lambda";
+import {
+  APIGatewayProxyResultV2,
+  APIGatewayProxyWebsocketEventV2,
+} from "aws-lambda";
+
+import { parseJSON } from "./json/parse";
+import { createAPIGatewayEndpoint } from "./websocket-api/api-gateway/endpoint";
+import { createApiGatewayManagementApi } from "./websocket-api/api-gateway/management";
+import { Notifier } from "./websocket-api/api-gateway/notifier";
+import { SendSDPSchema } from "./websocket-api/request/send-sdp";
+
+/** AWSリージョン */
+const AWS_REGION = process.env.AWS_REGION ?? "";
+/** ステージ */
+const STAGE = process.env.STAGE ?? "";
+/** Websocket API ID */
+const WEBSOCKET_API_ID = process.env.WEBSOCKET_API_ID ?? "";
+
+/** API エンドポイント */
+const apiGatewayEndpoint = createAPIGatewayEndpoint(
+  WEBSOCKET_API_ID,
+  AWS_REGION,
+  STAGE,
+);
+/** API Gateway Management API */
+const apiGateway = createApiGatewayManagementApi(apiGatewayEndpoint);
+/** WebSocket用メッセージ通知オブジェクト */
+const notifier = new Notifier(apiGateway);
 
 /**
  * SDPを相手に送信する
+ * @param event イベント
  * @returns レスポンス
  */
-export const sendSDP = async (): Promise<APIGatewayProxyResultV2> => {
+export const sendSDP = async (
+  event: APIGatewayProxyWebsocketEventV2,
+): Promise<APIGatewayProxyResultV2> => {
+  const { connectionId: guestConnectionId } = event.requestContext;
+  const parsedBody = parseJSON(event.body);
+  const parsedSendSDP = SendSDPSchema.safeParse(parsedBody);
+  if (parsedSendSDP.success === false) {
+    await notifier.notifyToClient(guestConnectionId, {
+      type: "send-sdp-rejected",
+    });
+    return { statusCode: 200, body: "invalid request" };
+  }
+
   return {
     statusCode: 200,
     body: "send sdp success",
--- packages/ws-signal/src/websocket-api/response/index.ts
+++ packages/ws-signal/src/websocket-api/response/index.ts
@@ -2,8 +2,16 @@ import { JoinRoomAccepted } from "./join-room-accepted";
 import { JoinRoomRejected } from "./join-room-rejected";
 import { Matching } from "./matching";
 import { Pong } from "./pong";
+import { ReceiveRemoteSDP } from "./recieve-remote-sdp";
 import { RoomCreationResult } from "./room-creation-result";
+import { SendSDPRejected } from "./send-sdp-rejected";
 
 /** websocketがクライアントに返すデータ */
 export type WebsocketResponse =
-  Pong | RoomCreationResult | JoinRoomAccepted | JoinRoomRejected | Matching;
+  | Pong
+  | RoomCreationResult
+  | JoinRoomAccepted
+  | JoinRoomRejected
+  | Matching
+  | ReceiveRemoteSDP
+  | SendSDPRejected;
