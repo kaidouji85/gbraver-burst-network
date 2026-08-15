d5899c8239c6345a947958f9c69b775de549c049
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-15 17:19:12 +0900

    fix: remove SendGuestSignal type and related functionality from signaling module

--- packages/local-webrtc-browser-sdk/src/websocket-api/request/index.ts
+++ packages/local-webrtc-browser-sdk/src/websocket-api/request/index.ts
@@ -1,9 +1,8 @@
 import { CreateRoom } from "./create-room";
 import { JoinRoom } from "./join-room";
-import { SendGuestSignal } from "./send-guest-signal";
 import { SendICECandidate } from "./send-ice-candidate";
 import { SendSDP } from "./send-sdp";
 
 /** WebSocketシグナルサーバーへのリクエスト */
 export type WSSignalRequest =
-  CreateRoom | JoinRoom | SendSDP | SendICECandidate | SendGuestSignal;
+  CreateRoom | JoinRoom | SendSDP | SendICECandidate;
