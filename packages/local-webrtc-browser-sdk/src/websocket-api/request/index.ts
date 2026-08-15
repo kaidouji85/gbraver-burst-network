241113fefbc2ce30eee979a815f6385bc9796497
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-15 13:25:47 +0900

    feat: add SendICECandidate type and ReceiveICECandidate response schema for ICE candidate handling

--- packages/local-webrtc-browser-sdk/src/websocket-api/request/index.ts
+++ packages/local-webrtc-browser-sdk/src/websocket-api/request/index.ts
@@ -1,7 +1,9 @@
 import { CreateRoom } from "./create-room";
 import { JoinRoom } from "./join-room";
 import { SendGuestSignal } from "./send-guest-signal";
+import { SendICECandidate } from "./send-ice-candidate";
 import { SendSDP } from "./send-sdp";
 
 /** WebSocketシグナルサーバーへのリクエスト */
-export type WSSignalRequest = CreateRoom | JoinRoom | SendSDP | SendGuestSignal;
+export type WSSignalRequest =
+  CreateRoom | JoinRoom | SendSDP | SendICECandidate | SendGuestSignal;
