42cb508894cd9d02caa224cf85f196dcca3e4373
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-15 12:42:49 +0900

    feat: add SendSDP and ReceiveRemoteSDP types for SDP signaling

--- packages/ws-signal/src/websocket-api/response/recieve-remote-sdp.ts
+++ packages/ws-signal/src/websocket-api/response/recieve-remote-sdp.ts
@@ -1,9 +1,4 @@
-import { z } from "zod";
-
-import {
-  RTCSessionDescriptionInit,
-  RTCSessionDescriptionInitSchema,
-} from "../../core/webrtc";
+import { RTCSessionDescriptionInit } from "../../core/webrtc";
 
 /** 相手からSDPを受信した */
 export type ReceiveRemoteSDP = {
@@ -13,10 +8,3 @@ export type ReceiveRemoteSDP = {
   /** SDP */
   sdp: RTCSessionDescriptionInit;
 };
-
-/** ReceiveRemoteSDP zod スキーマ */
-export const ReceiveRemoteSDPSchema = z.object({
-  type: z.literal("receive-remote-sdp"),
-  signalingID: z.string(),
-  sdp: RTCSessionDescriptionInitSchema,
-});
