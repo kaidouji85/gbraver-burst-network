9ebd0757ead3f498e18d286310ee8339e6459c66
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-17 08:57:14 +0900

    feat: update FrontendLog type to include SelectedIceCandidateSummary and reorganize signaling types

--- packages/local-webrtc-browser-sdk/src/http-api/request/frontend-log.ts
+++ packages/local-webrtc-browser-sdk/src/http-api/request/frontend-log.ts
@@ -1,3 +1,19 @@
+/** 選択されたICE Candidateの概要 */
+export type SelectedIceCandidateSummary = {
+  type: "SELECTED_ICE_CANDIDATE_SUMMARY";
+  /** ICE Candidate概要 */
+  summary: string;
+};
+
+/** ログ：ICE候補収集エラー */
+export type ICECandidateError = {
+  type: "ICE_CANDIDATE_ERROR";
+  /** スパンID */
+  spanId: string;
+  /** エラーメッセージ */
+  error: string;
+};
+
 /** ログ：シグナリング開始 */
 export type SignalingStart = {
   type: "SIGNALING_START";
@@ -12,33 +28,9 @@ export type SignalingEnd = {
   spanId: string;
 };
 
-/** ログ：ICE候補収集エラー */
-export type ICECandidateError = {
-  type: "ICE_CANDIDATE_ERROR";
-  /** スパンID */
-  spanId: string;
-  /** エラーメッセージ */
-  error: string;
-};
-
-/** ログ：ICE候補送信開始 */
-export type ICECandidateStart = {
-  type: "ICE_CANDIDATE_START";
-  /** スパンID */
-  spanId: string;
-};
-
-/** ログ：ICE候補送信終了 */
-export type ICECandidateEnd = {
-  type: "ICE_CANDIDATE_END";
-  /** スパンID */
-  spanId: string;
-};
-
 /** フロントエンドログ */
 export type FrontendLog =
-  | ICECandidateStart
-  | ICECandidateEnd
+  | SelectedIceCandidateSummary
   | ICECandidateError
   | SignalingStart
   | SignalingEnd;
