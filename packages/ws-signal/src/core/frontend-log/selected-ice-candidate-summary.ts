a23fd541b5370a9768716ab28e0e109c8e272ac6
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-18 08:15:49 +0900

    feat: add spanId field to SelectedIceCandidateSummary type and schema

--- packages/ws-signal/src/core/frontend-log/selected-ice-candidate-summary.ts
+++ packages/ws-signal/src/core/frontend-log/selected-ice-candidate-summary.ts
@@ -8,6 +8,8 @@ export const MAX_SUMMARY_LENGTH = 256;
 /** 選択されたICE Candidateの概要 */
 export type SelectedIceCandidateSummary = SpanIdContainer & {
   type: "SELECTED_ICE_CANDIDATE_SUMMARY";
+  /** スパンID */
+  spanId: string;
   /** ICE Candidate概要 */
   summary: string;
 };
@@ -15,5 +17,6 @@ export type SelectedIceCandidateSummary = SpanIdContainer & {
 /** SelectedIceCandidateSummary zodスキーマ */
 export const SelectedIceCandidateSummarySchema = SpanIdContainerSchema.extend({
   type: z.literal("SELECTED_ICE_CANDIDATE_SUMMARY"),
+  spanId: z.string(),
   summary: z.string().max(MAX_SUMMARY_LENGTH),
 });
