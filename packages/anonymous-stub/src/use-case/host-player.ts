25a75db351c0e77de6845e973880c1d07d412775
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-26 08:51:20 +0900

    feat: Implement WebRTC and WebSocket functionalities in anonymous-browser-sdk
    - Add functions to handle WebRTC signaling, including receiving and sending commands, waiting for connections, and managing data channels.
    - Introduce WebSocket API methods for creating and joining rooms, deleting signaling channels, and handling ICE candidates.
    - Define schemas for validating WebRTC and WebSocket messages using zod.
    - Create utility functions for notifying connection failures and waiting for specific events.
    - Update package dependencies and references to use the new anonymous-browser-sdk.

--- packages/anonymous-stub/src/use-case/host-player.ts
+++ packages/anonymous-stub/src/use-case/host-player.ts
@@ -1,4 +1,4 @@
-import { HostLocalWebRTCSDK } from "@gbraver-burst-network/local-webrtc-browser-sdk";
+import { HostLocalWebRTCSDK } from "@gbraver-burst-network/anonymous-browser-sdk";
 import { ArmdozerIds, PilotIds } from "gbraver-burst-core";
 
 import { waitTime } from "../wait-time";
