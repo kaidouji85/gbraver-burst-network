1740f7d39f96fb02088fcb4f18143f360f3e7e88
Y.Takeuchi <kaidouji85@gmail.com>
2026-08-28 22:14:16 +0900

    fix: ANONYMOUS_BACKEND_DOMAIN_NAMEを使用してWebRTCHelperDomainNameを更新

--- deploy-backend-cloudfont.bash
+++ deploy-backend-cloudfont.bash
@@ -5,14 +5,14 @@ OWN_PATH=$(cd "$(dirname "${0}")" && pwd)
 cd "${OWN_PATH}"/packages/cloudfront || exit
 
 STACK_NAME="${BACKEND_CLOUDFRONT_SERVICE:?}-g1"
-EXPORT_NAME_OF_WEB_RTC_HELPER_DOMAIN_NAME="${WS_SIGNAL_SERVICE:?}:${STAGE:?}:HttpApiDomainName"
-WEB_RTC_HELPER_DOMAIN_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='${EXPORT_NAME_OF_WEB_RTC_HELPER_DOMAIN_NAME}'].Value" --output text)
+EXPORT_NAME="${WS_SIGNAL_SERVICE:?}:${STAGE:?}:HttpApiDomainName"
+ANONYMOUS_BACKEND_DOMAIN_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='${EXPORT_NAME}'].Value" --output text)
 aws cloudformation deploy \
   --template-file gb-backend-cloudfront.yaml \
   --stack-name "${STACK_NAME}" \
   --region "${AWS_DEFAULT_REGION:?}" \
   --parameter-overrides \
-  WebRTCHelperDomainName="${WEB_RTC_HELPER_DOMAIN_NAME}" \
+  WebRTCHelperDomainName="${ANONYMOUS_BACKEND_DOMAIN_NAME}" \
   DistributionAlias="${BACKEND_CLOUDFRONT_DOMAIN_NAME:?}" \
   AcmCertificateArn="${BACKEND_CLOUDFRONT_CERT_ARN:?}" \
   WebACLArn="${BACKEND_CLOUDFRONT_WEB_ACL_ARN:-}" \
