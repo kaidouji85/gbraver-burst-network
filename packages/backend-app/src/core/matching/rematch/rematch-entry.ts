5eb3b3faa0c645da65d11dc15e4a11c3f77ba79c
Y.Takeuchi <kaidouji85@gmail.com>
2026-09-26 16:34:13 +0900

    feat: 再戦エントリにroomIDを追加し、スキーマを拡張

--- packages/backend-app/src/core/matching/rematch/rematch-entry.ts
+++ packages/backend-app/src/core/matching/rematch/rematch-entry.ts
@@ -1,7 +1,12 @@
 import { BattleEntry, BattleEntrySchema } from "../battle-entry";
+import { RematchRoomID, RematchRoomIDSchema } from "./rematch-room";
 
 /** 再戦エントリ */
-export type RematchEntry = BattleEntry;
+export type RematchEntry = BattleEntry & {
+  roomID: RematchRoomID;
+};
 
 /** RematchEntry zodスキーマ */
-export const RematchEntrySchema = BattleEntrySchema;
+export const RematchEntrySchema = BattleEntrySchema.extend({
+  roomID: RematchRoomIDSchema,
+});
