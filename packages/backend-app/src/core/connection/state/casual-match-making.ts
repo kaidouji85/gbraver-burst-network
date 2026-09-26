8e54f5b1dcaa19c936dcec9377e23e145f0f363d
Y.Takeuchi <kaidouji85@gmail.com>
2026-09-26 16:56:40 +0900

    refactor: reorder comments for better clarity in casual match making type

--- packages/backend-app/src/core/connection/state/casual-match-making.ts
+++ packages/backend-app/src/core/connection/state/casual-match-making.ts
@@ -3,8 +3,9 @@ import { z } from "zod";
 /** カジュアルマッチ マッチメイク中 */
 export type CasualMatchMaking = {
   type: "CasualMatchMaking";
-}; /** CasualMatchMaking zodスキーマ */
+};
 
+/** CasualMatchMaking zodスキーマ */
 export const CasualMatchMakingSchema = z.object({
   type: z.literal("CasualMatchMaking"),
 });
